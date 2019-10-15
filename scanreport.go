855c7bd3767207776e90141aa38c8dfc4b4150fc
Louis DeLosSantos <ldelossa@redhat.com>
2019-10-15 16:53:09 -0400

    add scanrecord and update scan report fields

--- scanreport.go
+++ scanreport.go
@@ -1,17 +1,54 @@
 package claircore
 
-// ScanReport aggregates the results of a container scan at the image layer and at the individual layer level.
+// ScanRecord represents a Package and it's associated Distribution and
+// Repository information
+type ScanRecord struct {
+	Package      *Package
+	Distribution *Distribution
+	Repository   *Repository
+}
+
+// ScanReport provides the found packages, distributions, and repositories in a layer and
+// their assocations with a particular package.
 type ScanReport struct {
 	// the manifest hash this scan result is assocaited with
 	Hash string `json:"manifest_hash"`
 	// the current state of the scan.
 	State string `json:"state"`
-	// packages found after stacking all layers
+	// packages found after applying all layers
 	Packages map[int]*Package `json:"packages"`
+	// distributions found after applying all layers
+	Distributions map[int]*Distribution `json:"distributions"`
+	// repositories found after applying all layers
+	Repositories map[int]*Repository `json:"repository"`
+	// PackagesByDistribution maps a package id to it's associated distribution id
+	DistributionByPackage map[int]int `json:"distributionByPackage"`
+	// PackagesByRepositories maps a package id to it's associated repository id
+	RepositoryByPackage map[int]int `json:"packagesByRepositories"`
 	// layer hash that introduced the given package id
-	PackageIntroduced map[int]string `json:"package_introduced"`
+	PackageIntroduced map[int]string `json:"packageIntroduced"`
 	// whether the scan was successful
 	Success bool `json:"success"`
 	// the first fatal error that occured during a scan process
 	Err string `json:"err"`
+}
+
+// ScanRecord seeks into the ScanReport and returns a ScanRecord when provided with a package
+func (report *ScanReport) ScanRecord(pkg *Package) (*ScanRecord, error) {
+	record := &ScanRecord{}
+	record.Package = pkg
+
+	if id, ok := report.DistributionByPackage[pkg.ID]; ok {
+		record.Distribution = report.Distributions[id]
+	} else {
+		record.Distribution = &Distribution{}
+	}
+
+	if id, ok := report.RepositoryByPackage[pkg.ID]; ok {
+		record.Repository = report.Repositories[id]
+	} else {
+		record.Repository = &Repository{}
+	}
+
+	return record, nil
 }
