42ef1cfd417d961b85cd593c51ad2e11fd411e62
Louis Delossantos <ldelossa.ld@gmail.com>
2019-10-15 21:19:46 -0400

    remove nameversion field in package

--- package.go
+++ package.go
@@ -4,8 +4,6 @@ type Package struct {
 	// unique ID of this package. this will be created as discovered by the library
 	// and used for persistence and hash map indexes
 	ID int `json:"id"`
-	// concatenated name:version string
-	NameVersion string `json:"name_version"`
 	// the name of the distribution
 	Name string `json:"name"`
 	// the version of the distribution
