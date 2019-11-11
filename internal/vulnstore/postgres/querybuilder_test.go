aba3e82979480bcf99c5b3ff484722a470a822eb
ldelossa <ldelossa.ld@gmail.com>
2019-11-11 11:24:00 -0500

    comments and formating

--- internal/vulnstore/postgres/querybuilder_test.go
+++ internal/vulnstore/postgres/querybuilder_test.go
@@ -7,7 +7,7 @@ import (
 	"github.com/quay/claircore/libvuln/driver"
 )
 
-func Test_GetBuilder_Determinist_Arguments(t *testing.T) {
+func Test_GetBuilder_Deterministic_Arguments(t *testing.T) {
 	var tt = []struct {
 		name     string
 		matchers []driver.MatchExp
@@ -53,33 +53,6 @@ func Test_GetBuilder_Determinist_Arguments(t *testing.T) {
 			matchers: []driver.MatchExp{driver.PackageDistributionName, driver.PackageDistributionDID},
 			expected: `SELECT "id", "name", "description", "links", "severity", "package_name", "package_version", "package_kind", "dist_id", "dist_name", "dist_version", "dist_version_code_name", "dist_version_id", "dist_arch", "dist_cpe", "dist_pretty_name", "repo_name", "repo_key", "repo_uri", "fixed_in_version" FROM "vuln" WHERE (("dist_name" = $1) AND ("dist_did" = $2) AND (("package_name" = $3) OR ("package_name" = $4)))`,
 		},
-		// {
-		// 	name: "expect dist_did, dist_name, dist_version",
-		// 	matchers: []driver.Matcher{
-		// 		driver.PackageDistributionDID,
-		// 		driver.PackageDistributionName,
-		// 		driver.PackageDistributionVersion,
-		// 	},
-		// 	expected: `SELECT "id", "name", "description", "links", "severity", "package_name", "package_version", "package_kind", "dist_id", "dist_name", "dist_version", "dist_version_code_name", "dist_version_id", "arch", "fixed_in_version" FROM "vuln" WHERE (("dist_did" = $1) AND ("dist_name" = $2) AND ("dist_version" = $3) AND ("package_name" = $4))`,
-		// },
-		// {
-		// 	name: "expect dist_name, dist_did, dist_version",
-		// 	matchers: []driver.Matcher{
-		// 		driver.PackageDistributionName,
-		// 		driver.PackageDistributionDID,
-		// 		driver.PackageDistributionVersion,
-		// 	},
-		// 	expected: `SELECT "id", "name", "description", "links", "severity", "package_name", "package_version", "package_kind", "dist_id", "dist_name", "dist_version", "dist_version_code_name", "dist_version_id", "arch", "fixed_in_version" FROM "vuln" WHERE (("dist_name" = $1) AND ("dist_did" = $2) AND ("dist_version" = $3) AND ("package_name" = $4))`,
-		// },
-		// {
-		// 	name: "expect dist_version, dist_did, dist_name",
-		// 	matchers: []driver.Matcher{
-		// 		driver.PackageDistributionVersion,
-		// 		driver.PackageDistributionDID,
-		// 		driver.PackageDistributionName,
-		// 	},
-		// 	expected: `SELECT "id", "name", "description", "links", "severity", "package_name", "package_version", "package_kind", "dist_id", "dist_name", "dist_version", "dist_version_code_name", "dist_version_id", "arch", "fixed_in_version" FROM "vuln" WHERE (("dist_version" = $1) AND ("dist_did" = $2) AND ("dist_name" = $3) AND ("package_name" = $4))`,
-		// },
 	}
 
 	for _, table := range tt {
@@ -89,7 +62,6 @@ func Test_GetBuilder_Determinist_Arguments(t *testing.T) {
 				t.Fatalf("failed to build query: %v", err)
 			}
 
-			// assert.Equal(t, table.expected, q)
 			if !cmp.Equal(table.expected, q) {
 				t.Fatalf("%v", cmp.Diff(table.expected, q))
 			}
