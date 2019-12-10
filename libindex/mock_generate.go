05ab286210f9c2ee73651649addf68d5e9d0e1cf
Louis DeLosSantos <ldelossa@redhat.com>
2019-12-10 10:46:42 -0500

    remove interfaces, remove channel usage

--- libindex/mock_generate.go
+++
@@ -1,3 +0,0 @@
-package libindex
-
-//go:generate mockgen -package=libindex -self_package=github.com/quay/claircore/libindex -destination=./libindex_mock.go github.com/quay/claircore/libindex Libindex
