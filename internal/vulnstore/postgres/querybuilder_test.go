7805d4fb6cc584878072cc3cdd66a6f589f6192b
Louis DeLosSantos <ldelossa@redhat.com>
2019-12-11 14:46:39 -0500

    update tests

--- internal/vulnstore/postgres/querybuilder_test.go
+++ internal/vulnstore/postgres/querybuilder_test.go
@@ -4,42 +4,98 @@ import (
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
-
+	"github.com/quay/claircore"
 	"github.com/quay/claircore/libvuln/driver"
+	"github.com/quay/claircore/test"
 )
 
-func Test_GetBuilder_Deterministic_Arguments(t *testing.T) {
-	var tt = []struct {
-		name     string
-		matchers []driver.MatchExp
-		expected string
+func Test_GetQueryBuilder_Deterministic_Args(t *testing.T) {
+	var table = []struct {
+		// name of test
+		name string
+		// the expected query string returned
+		expectedQuery string
+		// the match expressions which contrain the query
+		matchExps []driver.MatchExp
+		// a method to returning the indexRecord for the getQueryBuilder method
+		indexRecord func() *claircore.IndexRecord
 	}{
 		{
-			name:     "expect dist_did",
-			matchers: []driver.MatchExp{driver.DistributionDID},
-			expected: `SELECT "id", "name", "description", "links", "severity", "package_name", "package_version", "package_kind", "dist_id", "dist_name", "dist_version", "dist_version_code_name", "dist_version_id", "dist_arch", "dist_cpe", "dist_pretty_name", "repo_name", "repo_key", "repo_uri", "fixed_in_version" FROM "vuln" WHERE ((("package_name" = $1) OR ("package_name" = $2)) AND ("dist_did" = $3))`,
+			name:          "No source package constrained by dist_id",
+			expectedQuery: `SELECT "id", "name", "description", "links", "severity", "package_name", "package_version", "package_kind", "dist_id", "dist_name", "dist_version", "dist_version_code_name", "dist_version_id", "dist_arch", "dist_cpe", "dist_pretty_name", "repo_name", "repo_key", "repo_uri", "fixed_in_version" FROM "vuln" WHERE (("package_name" = 'package-0') AND ("dist_id" = 'did-0'))`,
+			matchExps:     []driver.MatchExp{driver.DistributionDID},
+			indexRecord: func() *claircore.IndexRecord {
+				pkgs := test.GenUniquePackages(1)
+				pkgs[0].Source = &claircore.Package{} // clear source field
+				dists := test.GenUniqueDistributions(1)
+				return &claircore.IndexRecord{
+					Package:      pkgs[0],
+					Distribution: dists[0],
+				}
+			},
 		},
 		{
-			name:     "expect dist_did, dist_name",
-			matchers: []driver.MatchExp{driver.DistributionDID, driver.DistributionName},
-			expected: `SELECT "id", "name", "description", "links", "severity", "package_name", "package_version", "package_kind", "dist_id", "dist_name", "dist_version", "dist_version_code_name", "dist_version_id", "dist_arch", "dist_cpe", "dist_pretty_name", "repo_name", "repo_key", "repo_uri", "fixed_in_version" FROM "vuln" WHERE ((("package_name" = $1) OR ("package_name" = $2)) AND ("dist_did" = $3) AND ("dist_name" = $4))`,
+			name:          "Source package constrained by dist_id",
+			expectedQuery: `SELECT "id", "name", "description", "links", "severity", "package_name", "package_version", "package_kind", "dist_id", "dist_name", "dist_version", "dist_version_code_name", "dist_version_id", "dist_arch", "dist_cpe", "dist_pretty_name", "repo_name", "repo_key", "repo_uri", "fixed_in_version" FROM "vuln" WHERE ((("package_name" = 'package-0') OR ("package_name" = 'source-package-0')) AND ("dist_id" = 'did-0'))`,
+			matchExps:     []driver.MatchExp{driver.DistributionDID},
+			indexRecord: func() *claircore.IndexRecord {
+				pkgs := test.GenUniquePackages(1)
+				dists := test.GenUniqueDistributions(1)
+				return &claircore.IndexRecord{
+					Package:      pkgs[0],
+					Distribution: dists[0],
+				}
+			},
 		},
 		{
-			name:     "expect dist_name, dist_did",
-			matchers: []driver.MatchExp{driver.DistributionName, driver.DistributionDID},
-			expected: `SELECT "id", "name", "description", "links", "severity", "package_name", "package_version", "package_kind", "dist_id", "dist_name", "dist_version", "dist_version_code_name", "dist_version_id", "dist_arch", "dist_cpe", "dist_pretty_name", "repo_name", "repo_key", "repo_uri", "fixed_in_version" FROM "vuln" WHERE ((("package_name" = $1) OR ("package_name" = $2)) AND ("dist_name" = $3) AND ("dist_did" = $4))`,
+			name:          "Source package constrained by dist_id",
+			expectedQuery: `SELECT "id", "name", "description", "links", "severity", "package_name", "package_version", "package_kind", "dist_id", "dist_name", "dist_version", "dist_version_code_name", "dist_version_id", "dist_arch", "dist_cpe", "dist_pretty_name", "repo_name", "repo_key", "repo_uri", "fixed_in_version" FROM "vuln" WHERE ((("package_name" = 'package-0') OR ("package_name" = 'source-package-0')) AND ("dist_id" = 'did-0') AND ("dist_version" = 'version-0'))`,
+			matchExps:     []driver.MatchExp{driver.DistributionDID, driver.DistributionVersion},
+			indexRecord: func() *claircore.IndexRecord {
+				pkgs := test.GenUniquePackages(1)
+				dists := test.GenUniqueDistributions(1)
+				return &claircore.IndexRecord{
+					Package:      pkgs[0],
+					Distribution: dists[0],
+				}
+			},
+		},
+		{
+			name:          "Source package constrained by dist_id",
+			expectedQuery: `SELECT "id", "name", "description", "links", "severity", "package_name", "package_version", "package_kind", "dist_id", "dist_name", "dist_version", "dist_version_code_name", "dist_version_id", "dist_arch", "dist_cpe", "dist_pretty_name", "repo_name", "repo_key", "repo_uri", "fixed_in_version" FROM "vuln" WHERE ((("package_name" = 'package-0') OR ("package_name" = 'source-package-0')) AND ("dist_id" = 'did-0') AND ("dist_version" = 'version-0') AND ("dist_version_id" = 'version-id-0'))`,
+			matchExps:     []driver.MatchExp{driver.DistributionDID, driver.DistributionVersion, driver.DistributionVersionID},
+			indexRecord: func() *claircore.IndexRecord {
+				pkgs := test.GenUniquePackages(1)
+				dists := test.GenUniqueDistributions(1)
+				return &claircore.IndexRecord{
+					Package:      pkgs[0],
+					Distribution: dists[0],
+				}
+			},
+		},
+		{
+			name:          "Source package constrained by dist_id",
+			expectedQuery: `SELECT "id", "name", "description", "links", "severity", "package_name", "package_version", "package_kind", "dist_id", "dist_name", "dist_version", "dist_version_code_name", "dist_version_id", "dist_arch", "dist_cpe", "dist_pretty_name", "repo_name", "repo_key", "repo_uri", "fixed_in_version" FROM "vuln" WHERE ((("package_name" = 'package-0') OR ("package_name" = 'source-package-0')) AND ("dist_id" = 'did-0') AND ("dist_version" = 'version-0') AND ("dist_version_id" = 'version-id-0') AND ("dist_version_code_name" = 'version-code-name-0'))`,
+			matchExps:     []driver.MatchExp{driver.DistributionDID, driver.DistributionVersion, driver.DistributionVersionID, driver.DistributionVersionCodeName},
+			indexRecord: func() *claircore.IndexRecord {
+				pkgs := test.GenUniquePackages(1)
+				dists := test.GenUniqueDistributions(1)
+				return &claircore.IndexRecord{
+					Package:      pkgs[0],
+					Distribution: dists[0],
+				}
+			},
 		},
 	}
-
-	for _, table := range tt {
-		t.Run(table.name, func(t *testing.T) {
-			q, _, err := getBuilder(table.matchers)
+	for _, tt := range table {
+		t.Run(tt.name, func(t *testing.T) {
+			ir := tt.indexRecord()
+			query, err := getQueryBuilder(ir, tt.matchExps)
 			if err != nil {
-				t.Fatalf("failed to build query: %v", err)
+				t.Fatalf("failed to create query: %v", err)
 			}
-
-			if !cmp.Equal(table.expected, q) {
-				t.Fatalf("%v", cmp.Diff(table.expected, q))
+			if !cmp.Equal(query, tt.expectedQuery) {
+				t.Fatalf("%v", cmp.Diff(query, tt.expectedQuery))
 			}
 		})
 	}
