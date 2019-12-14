200446903beb7cddb447524c74ab296d9f7c9c60
ldelossa <ldelossa.ld@gmail.com>
2019-12-13 23:44:53 -0500

    update tests and docs

--- ubuntu/matcher_integration_test.go
+++ ubuntu/matcher_integration_test.go
@@ -11,8 +11,8 @@ import (
 	"github.com/stretchr/testify/assert"
 
 	"github.com/quay/claircore"
+	"github.com/quay/claircore/internal/matcher"
 	"github.com/quay/claircore/internal/updater"
-	"github.com/quay/claircore/internal/vulnscanner"
 	vulnstore "github.com/quay/claircore/internal/vulnstore/postgres"
 	"github.com/quay/claircore/libvuln/driver"
 	distlock "github.com/quay/claircore/pkg/distlock/postgres"
@@ -28,12 +28,9 @@ func Test_Matcher_Integration(t *testing.T) {
 	ctx := context.Background()
 	db, store, _, teardown := vulnstore.TestStore(ctx, t)
 	defer teardown()
-
 	m := &Matcher{}
-
 	// seed the test vulnstore with CVE data
 	ub := NewUpdater(Bionic)
-
 	up := updater.New(&updater.Opts{
 		Name:    "test-ubuntu-bionic",
 		Updater: ub,
@@ -46,23 +43,18 @@ func Test_Matcher_Integration(t *testing.T) {
 	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Minute)
 	defer cancel()
 	up.Update(ctx)
-
 	path := filepath.Join("testdata", "indexreport-bionic.json")
 	f, err := os.Open(path)
 	if err != nil {
 		t.Fatalf("%v", err)
 	}
-
-	var sr claircore.IndexReport
-	err = json.NewDecoder(f).Decode(&sr)
+	var ir claircore.IndexReport
+	err = json.NewDecoder(f).Decode(&ir)
 	if err != nil {
 		t.Fatalf("failed to decode IndexReport: %v", err)
 	}
-
-	vs := vulnscanner.New(store, []driver.Matcher{m})
-	vr, err := vs.Scan(context.Background(), &sr)
+	vr, err := matcher.Match(context.Background(), &ir, []driver.Matcher{m}, store)
 	assert.NoError(t, err)
-
 	_, err = json.Marshal(&vr)
 	if err != nil {
 		t.Fatalf("failed to marshal VR: %v", err)
