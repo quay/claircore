bd9918af1074450541e9d23d5da2ab519f3adbd6
ldelossa <ldelossa.ld@gmail.com>
2019-12-13 23:44:53 -0500

    update tests and docs

--- debian/matcher_integration_test.go
+++ debian/matcher_integration_test.go
@@ -9,13 +9,14 @@ import (
 	"time"
 
 	"github.com/quay/claircore"
+	"github.com/quay/claircore/internal/matcher"
 	"github.com/quay/claircore/internal/updater"
-	"github.com/quay/claircore/internal/vulnscanner"
 	vulnstore "github.com/quay/claircore/internal/vulnstore/postgres"
 	"github.com/quay/claircore/libvuln/driver"
 	distlock "github.com/quay/claircore/pkg/distlock/postgres"
 	"github.com/quay/claircore/test/integration"
 	"github.com/quay/claircore/test/log"
+	"github.com/stretchr/testify/assert"
 )
 
 // Test_Matcher_Integration confirms packages are matched
@@ -29,12 +30,9 @@ func Test_Matcher_Integration(t *testing.T) {
 	ctx, _ = log.TestLogger(ctx, t)
 	db, store, _, teardown := vulnstore.TestStore(ctx, t)
 	defer teardown()
-
 	m := &Matcher{}
-
 	// seed the test vulnstore with CVE data
 	deb := NewUpdater(Buster)
-
 	up := updater.New(&updater.Opts{
 		Name:    "test-debian-buster",
 		Updater: deb,
@@ -47,25 +45,19 @@ func Test_Matcher_Integration(t *testing.T) {
 	ctx, cancel := context.WithTimeout(ctx, 2*time.Minute)
 	defer cancel()
 	up.Update(ctx)
-
 	path := filepath.Join("testdata", "indexreport-buster-jackson-databind.json")
 	f, err := os.Open(path)
 	if err != nil {
 		t.Fatalf("%v", err)
 	}
 
-	var sr claircore.IndexReport
-	err = json.NewDecoder(f).Decode(&sr)
+	var ir claircore.IndexReport
+	err = json.NewDecoder(f).Decode(&ir)
 	if err != nil {
 		t.Fatalf("failed to decode IndexReport: %v", err)
 	}
-
-	vs := vulnscanner.New(store, []driver.Matcher{m})
-	vr, err := vs.Scan(ctx, &sr)
-	if err != nil {
-		t.Error(err)
-	}
-
+	vr, err := matcher.Match(context.Background(), &ir, []driver.Matcher{m}, store)
+	assert.NoError(t, err)
 	_, err = json.Marshal(&vr)
 	if err != nil {
 		t.Fatalf("failed to marshal VR: %v", err)
--- ubuntu/matcher_integration_test.go
+++ ubuntu/matcher_integration_test.go
@@ -9,13 +9,14 @@ import (
 	"time"
 
 	"github.com/quay/claircore"
+	"github.com/quay/claircore/internal/matcher"
 	"github.com/quay/claircore/internal/updater"
-	"github.com/quay/claircore/internal/vulnscanner"
 	vulnstore "github.com/quay/claircore/internal/vulnstore/postgres"
 	"github.com/quay/claircore/libvuln/driver"
 	distlock "github.com/quay/claircore/pkg/distlock/postgres"
 	"github.com/quay/claircore/test/integration"
 	"github.com/quay/claircore/test/log"
+	"github.com/stretchr/testify/assert"
 )
 
 // Test_Matcher_Integration confirms packages are matched
@@ -29,12 +30,9 @@ func Test_Matcher_Integration(t *testing.T) {
 	ctx, _ = log.TestLogger(ctx, t)
 	db, store, _, teardown := vulnstore.TestStore(ctx, t)
 	defer teardown()
-
 	m := &Matcher{}
-
 	// seed the test vulnstore with CVE data
 	ub := NewUpdater(Bionic)
-
 	up := updater.New(&updater.Opts{
 		Name:    "test-ubuntu-bionic",
 		Updater: ub,
@@ -47,25 +45,18 @@ func Test_Matcher_Integration(t *testing.T) {
 	ctx, cancel := context.WithTimeout(ctx, 2*time.Minute)
 	defer cancel()
 	up.Update(ctx)
-
 	path := filepath.Join("testdata", "indexreport-bionic.json")
 	f, err := os.Open(path)
 	if err != nil {
 		t.Fatalf("%v", err)
 	}
-
-	var sr claircore.IndexReport
-	err = json.NewDecoder(f).Decode(&sr)
+	var ir claircore.IndexReport
+	err = json.NewDecoder(f).Decode(&ir)
 	if err != nil {
 		t.Fatalf("failed to decode IndexReport: %v", err)
 	}
-
-	vs := vulnscanner.New(store, []driver.Matcher{m})
-	vr, err := vs.Scan(ctx, &sr)
-	if err != nil {
-		t.Fatal(err)
-	}
-
+	vr, err := matcher.Match(context.Background(), &ir, []driver.Matcher{m}, store)
+	assert.NoError(t, err)
 	_, err = json.Marshal(&vr)
 	if err != nil {
 		t.Fatalf("failed to marshal VR: %v", err)
