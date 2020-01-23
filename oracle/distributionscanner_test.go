3d88ce9365bbc94d0537499bf38a33707b965e89
ldelossa <ldelossa.ld@gmail.com>
2020-01-23 12:45:12 -0500

    remove unhelpful tests

--- oracle/distributionscanner_test.go
+++ oracle/distributionscanner_test.go
@@ -26,25 +26,6 @@ ORACLE_BUGZILLA_PRODUCT_VERSION=8.0
 ORACLE_SUPPORT_PRODUCT="Oracle Linux"
 ORACLE_SUPPORT_PRODUCT_VERSION=8.0`)
 
-var eightOSReleaseBad []byte = []byte(`NAME="Oracle Linux Server"
-VERSION="8.0"
-ID="ol"
-ID_LIKE="fedora"
-VARIANT="Server"
-VARIANT_ID="server"
-VERSION_ID="8.0"
-PLATFORM_ID="platform:el8"
-PRETTY_NAME="Oracle Linux Server"
-ANSI_COLOR="0;31"
-CPE_NAME="cpe:/o:oracle:linux:8:0:server"
-HOME_URL="https://linux.oracle.com/"
-BUG_REPORT_URL="https://bugzilla.oracle.com/"
-
-ORACLE_BUGZILLA_PRODUCT="Oracle Linux 8"
-ORACLE_BUGZILLA_PRODUCT_VERSION=8.0
-ORACLE_SUPPORT_PRODUCT="Oracle Linux"
-ORACLE_SUPPORT_PRODUCT_VERSION=8.0`)
-
 var sevenOSRelease []byte = []byte(`NAME="Oracle Linux Server"
 VERSION="7.7"
 ID="ol"
@@ -53,24 +34,6 @@ VARIANT="Server"
 VARIANT_ID="server"
 VERSION_ID="7.7"
 PRETTY_NAME="Oracle Linux Server 7.7"
-ANSI_COLOR="0;31"
-CPE_NAME="cpe:/o:oracle:linux:7:7:server"
-HOME_URL="https://linux.oracle.com/"
-BUG_REPORT_URL="https://bugzilla.oracle.com/"
-
-ORACLE_BUGZILLA_PRODUCT="Oracle Linux 7"
-ORACLE_BUGZILLA_PRODUCT_VERSION=7.7
-ORACLE_SUPPORT_PRODUCT="Oracle Linux"
-ORACLE_SUPPORT_PRODUCT_VERSION=7.7`)
-
-var sevenOSReleaseBad []byte = []byte(`NAME="Oracle Linux Server"
-VERSION="7.7"
-ID="ol"
-ID_LIKE="fedora"
-VARIANT="Server"
-VARIANT_ID="server"
-VERSION_ID="7.7"
-PRETTY_NAME="Oracle Linux Server"
 ANSI_COLOR="0;31"
 CPE_NAME="cpe:/o:oracle:linux:7:7:server"
 HOME_URL="https://linux.oracle.com/"
@@ -96,29 +59,9 @@ ORACLE_BUGZILLA_PRODUCT_VERSION=6.10
 ORACLE_SUPPORT_PRODUCT="Oracle Linux"
 ORACLE_SUPPORT_PRODUCT_VERSION=6.10`)
 
-var sixOSReleaseBad []byte = []byte(`NAME="Oracle Linux Server"
-VERSION="6.10"
-ID="ol"
-VERSION_ID="6.10"
-PRETTY_NAME="Oracle Linux Server"
-ANSI_COLOR="0;31"
-CPE_NAME="cpe:/o:oracle:linux:6:10:server"
-HOME_URL="https://linux.oracle.com/"
-BUG_REPORT_URL="https://bugzilla.oracle.com/"
-
-ORACLE_BUGZILLA_PRODUCT="Oracle Linux 6"
-ORACLE_BUGZILLA_PRODUCT_VERSION=6.10
-ORACLE_SUPPORT_PRODUCT="Oracle Linux"
-ORACLE_SUPPORT_PRODUCT_VERSION=6.10`)
-
 // Oracle Five versions do not have os-release file and only have /etc/issue file
 var fiveIssue []byte = []byte(`
 Oracle Linux Server release 5.11
-Kernel \r on an \m
-`)
-
-var fiveIssueBad []byte = []byte(`
-Oracle Linux Server release
 Kernel \r on an \m
 `)
 
@@ -155,44 +98,6 @@ func TestDistributionScanner(t *testing.T) {
 			dist := scanner.parse(bytes.NewBuffer(tt.file))
 			if !cmp.Equal(dist, releaseToDist(tt.release)) {
 				t.Fatalf("%v", cmp.Diff(dist, releaseToDist(tt.release)))
-			}
-		})
-	}
-}
-
-func TestDistributionScannerBad(t *testing.T) {
-	table := []struct {
-		name    string
-		release Release
-		file    []byte
-	}{
-		{
-			name:    "8.0",
-			release: Eight,
-			file:    eightOSReleaseBad,
-		},
-		{
-			name:    "7.7",
-			release: Seven,
-			file:    sevenOSReleaseBad,
-		},
-		{
-			name:    "6.10",
-			release: Six,
-			file:    sixOSReleaseBad,
-		},
-		{
-			name:    "5.11",
-			release: Five,
-			file:    fiveIssueBad,
-		},
-	}
-	for _, tt := range table {
-		t.Run(tt.name, func(t *testing.T) {
-			scanner := DistributionScanner{}
-			dist := scanner.parse(bytes.NewBuffer(tt.file))
-			if dist != nil {
-				t.Fatalf("expected nil dist got %v", dist)
 			}
 		})
 	}
--- ubuntu/distributionscanner_test.go
+++ ubuntu/distributionscanner_test.go
@@ -21,27 +21,9 @@ PRIVACY_POLICY_URL="https://www.ubuntu.com/legal/terms-and-policies/privacy-poli
 VERSION_CODENAME=artful
 UBUNTU_CODENAME=artful`)
 
-var artfulOSReleaseBad []byte = []byte(`NAME="Ubuntu"
-VERSION="17.10 ( Aardvark)"
-ID=ubuntu
-ID_LIKE=debian
-PRETTY_NAME="Ubuntu 17.10"
-VERSION_ID="17.10"
-HOME_URL="https://www.ubuntu.com/"
-SUPPORT_URL="https://help.ubuntu.com/"
-BUG_REPORT_URL="https://bugs.launchpad.net/ubuntu/"
-PRIVACY_POLICY_URL="https://www.ubuntu.com/legal/terms-and-policies/privacy-policy"
-VERSION_CODENAME=
-UBUNTU_CODENAME=`)
-
 var artfulLSBRelease []byte = []byte(`DISTRIB_ID=Ubuntu
 DISTRIB_RELEASE=17.10
 DISTRIB_CODENAME=artful
-DISTRIB_DESCRIPTION="Ubuntu 17.10"`)
-
-var artfulLSBReleaseBad []byte = []byte(`DISTRIB_ID=Ubuntu
-DISTRIB_RELEASE=17.10
-DISTRIB_CODENAME=
 DISTRIB_DESCRIPTION="Ubuntu 17.10"`)
 
 // bionic test data
@@ -58,27 +40,9 @@ PRIVACY_POLICY_URL="https://www.ubuntu.com/legal/terms-and-policies/privacy-poli
 VERSION_CODENAME=bionic
 UBUNTU_CODENAME=bionic`)
 
-var bionicOSReleaseBad []byte = []byte(`NAME="Ubuntu"
-VERSION="18.04.3 LTS ( Beaver)"
-ID=ubuntu
-ID_LIKE=debian
-PRETTY_NAME="Ubuntu 18.04.3 LTS"
-VERSION_ID="18.04"
-HOME_URL="https://www.ubuntu.com/"
-SUPPORT_URL="https://help.ubuntu.com/"
-BUG_REPORT_URL="https://bugs.launchpad.net/ubuntu/"
-PRIVACY_POLICY_URL="https://www.ubuntu.com/legal/terms-and-policies/privacy-policy"
-VERSION_CODENAME=
-UBUNTU_CODENAME=`)
-
 var bionicLSBRelease []byte = []byte(`DISTRIB_ID=Ubuntu
 DISTRIB_RELEASE=18.04
 DISTRIB_CODENAME=bionic
-DISTRIB_DESCRIPTION="Ubuntu 18.04.3 LTS"`)
-
-var bionicLSBReleaseBad []byte = []byte(`DISTRIB_ID=Ubuntu
-DISTRIB_RELEASE=18.04
-DISTRIB_CODENAME=
 DISTRIB_DESCRIPTION="Ubuntu 18.04.3 LTS"`)
 
 // cosmic test data
@@ -95,27 +59,9 @@ PRIVACY_POLICY_URL="https://www.ubuntu.com/legal/terms-and-policies/privacy-poli
 VERSION_CODENAME=cosmic
 UBUNTU_CODENAME=cosmic`)
 
-var cosmicOSReleaseBad []byte = []byte(`NAME="Ubuntu"
-VERSION="18.10 ( Cuttlefish)"
-ID=ubuntu
-ID_LIKE=debian
-PRETTY_NAME="Ubuntu 18.10"
-VERSION_ID="18.10"
-HOME_URL="https://www.ubuntu.com/"
-SUPPORT_URL="https://help.ubuntu.com/"
-BUG_REPORT_URL="https://bugs.launchpad.net/ubuntu/"
-PRIVACY_POLICY_URL="https://www.ubuntu.com/legal/terms-and-policies/privacy-policy"
-VERSION_CODENAME=
-UBUNTU_CODENAME=`)
-
 var cosmicLSBRelease []byte = []byte(`DISTRIB_ID=Ubuntu
 DISTRIB_RELEASE=18.10
 DISTRIB_CODENAME=cosmic
-DISTRIB_DESCRIPTION="Ubuntu 18.10"`)
-
-var cosmicLSBReleaseBad []byte = []byte(`DISTRIB_ID=Ubuntu
-DISTRIB_RELEASE=18.10
-DISTRIB_CODENAME=
 DISTRIB_DESCRIPTION="Ubuntu 18.10"`)
 
 // disco test data
@@ -132,27 +78,9 @@ PRIVACY_POLICY_URL="https://www.ubuntu.com/legal/terms-and-policies/privacy-poli
 VERSION_CODENAME=disco
 UBUNTU_CODENAME=disco`)
 
-var discoOSReleaseBad []byte = []byte(`NAME="Ubuntu"
-VERSION="19.04 ( Dingo)"
-ID=ubuntu
-ID_LIKE=debian
-PRETTY_NAME="Ubuntu 19.04"
-VERSION_ID="19.04"
-HOME_URL="https://www.ubuntu.com/"
-SUPPORT_URL="https://help.ubuntu.com/"
-BUG_REPORT_URL="https://bugs.launchpad.net/ubuntu/"
-PRIVACY_POLICY_URL="https://www.ubuntu.com/legal/terms-and-policies/privacy-policy"
-VERSION_CODENAME=
-UBUNTU_CODENAME=`)
-
 var discoLSBRelease []byte = []byte(`DISTRIB_ID=Ubuntu
 DISTRIB_RELEASE=19.04
 DISTRIB_CODENAME=disco
-DISTRIB_DESCRIPTION="Ubuntu 19.04"`)
-
-var discoLSBReleaseBad []byte = []byte(`DISTRIB_ID=Ubuntu
-DISTRIB_RELEASE=19.04
-DISTRIB_CODENAME=
 DISTRIB_DESCRIPTION="Ubuntu 19.04"`)
 
 // precise test data
@@ -163,21 +91,9 @@ ID_LIKE=debian
 PRETTY_NAME="Ubuntu precise (12.04.5 LTS)"
 VERSION_ID="12.04"`)
 
-var preciseOSReleaseBad []byte = []byte(`NAME="Ubuntu"
-VERSION="12.04.5 LTS, Pangolin"
-ID=ubuntu
-ID_LIKE=debian
-PRETTY_NAME="Ubuntu (12.04.5 LTS)"
-VERSION_ID="12.04"`)
-
 var preciseLSBRelease []byte = []byte(`DISTRIB_ID=Ubuntu
 DISTRIB_RELEASE=12.04
 DISTRIB_CODENAME=precise
-DISTRIB_DESCRIPTION="Ubuntu 12.04.5 LTS"`)
-
-var preciseLSBReleaseBad []byte = []byte(`DISTRIB_ID=Ubuntu
-DISTRIB_RELEASE=12.04
-DISTRIB_CODENAME=
 DISTRIB_DESCRIPTION="Ubuntu 12.04.5 LTS"`)
 
 // trusty test data
@@ -191,24 +107,9 @@ HOME_URL="http://www.ubuntu.com/"
 SUPPORT_URL="http://help.ubuntu.com/"
 BUG_REPORT_URL="http://bugs.launchpad.net/ubuntu/"`)
 
-var trustyOSReleaseBad []byte = []byte(`NAME="Ubuntu"
-VERSION="14.04.6 LTS, Tahr"
-ID=ubuntu
-ID_LIKE=debian
-PRETTY_NAME="Ubuntu 14.04.6 LTS"
-VERSION_ID="14.04"
-HOME_URL="http://www.ubuntu.com/"
-SUPPORT_URL="http://help.ubuntu.com/"
-BUG_REPORT_URL="http://bugs.launchpad.net/ubuntu/"`)
-
 var trustyLSBRelease []byte = []byte(`DISTRIB_ID=Ubuntu
 DISTRIB_RELEASE=14.04
 DISTRIB_CODENAME=trusty
-DISTRIB_DESCRIPTION="Ubuntu 14.04.6 LTS"`)
-
-var trustyLSBReleaseBad []byte = []byte(`DISTRIB_ID=Ubuntu
-DISTRIB_RELEASE=14.04
-DISTRIB_CODENAME=
 DISTRIB_DESCRIPTION="Ubuntu 14.04.6 LTS"`)
 
 // xenial test data
@@ -224,26 +125,9 @@ BUG_REPORT_URL="http://bugs.launchpad.net/ubuntu/"
 VERSION_CODENAME=xenial
 UBUNTU_CODENAME=xenial`)
 
-var xenialOSReleaseBad []byte = []byte(`NAME="Ubuntu"
-VERSION="16.04.6 LTS (Xerus)"
-ID=ubuntu
-ID_LIKE=debian
-PRETTY_NAME="Ubuntu 16.04.6 LTS"
-VERSION_ID="16.04"
-HOME_URL="http://www.ubuntu.com/"
-SUPPORT_URL="http://help.ubuntu.com/"
-BUG_REPORT_URL="http://bugs.launchpad.net/ubuntu/"
-VERSION_CODENAME=
-UBUNTU_CODENAME=`)
-
 var xenialLSBRelease []byte = []byte(`DISTRIB_ID=Ubuntu
 DISTRIB_RELEASE=16.04
 DISTRIB_CODENAME=xenial
-DISTRIB_DESCRIPTION="Ubuntu 16.04.6 LTS"`)
-
-var xenialLSBReleaseBad []byte = []byte(`DISTRIB_ID=Ubuntu
-DISTRIB_RELEASE=16.04
-DISTRIB_CODENAME=
 DISTRIB_DESCRIPTION="Ubuntu 16.04.6 LTS"`)
 
 func TestDistributionScanner(t *testing.T) {
@@ -306,71 +190,6 @@ func TestDistributionScanner(t *testing.T) {
 			dist = scanner.parse(bytes.NewBuffer(tt.lsbRelease))
 			if !cmp.Equal(dist, releaseToDist(tt.release)) {
 				t.Fatalf("%v", cmp.Diff(dist, releaseToDist(tt.release)))
-			}
-		})
-	}
-}
-
-func TestDistributionScannerBad(t *testing.T) {
-	table := []struct {
-		name       string
-		release    Release
-		osRelease  []byte
-		lsbRelease []byte
-	}{
-		{
-			name:       "artful",
-			release:    Artful,
-			osRelease:  artfulOSReleaseBad,
-			lsbRelease: artfulLSBReleaseBad,
-		},
-		{
-			name:       "bionic",
-			release:    Bionic,
-			osRelease:  bionicOSReleaseBad,
-			lsbRelease: bionicLSBReleaseBad,
-		},
-		{
-			name:       "cosmic",
-			release:    Cosmic,
-			osRelease:  cosmicOSReleaseBad,
-			lsbRelease: cosmicLSBReleaseBad,
-		},
-		{
-			name:       "disco",
-			release:    Disco,
-			osRelease:  discoOSReleaseBad,
-			lsbRelease: discoLSBReleaseBad,
-		},
-		{
-			name:       "precise",
-			release:    Precise,
-			osRelease:  preciseOSReleaseBad,
-			lsbRelease: preciseLSBReleaseBad,
-		},
-		{
-			name:       "trusty",
-			release:    Trusty,
-			osRelease:  trustyOSReleaseBad,
-			lsbRelease: trustyLSBReleaseBad,
-		},
-		{
-			name:       "xenial",
-			release:    Xenial,
-			osRelease:  xenialOSReleaseBad,
-			lsbRelease: xenialLSBReleaseBad,
-		},
-	}
-	for _, tt := range table {
-		t.Run(tt.name, func(t *testing.T) {
-			scanner := DistributionScanner{}
-			dist := scanner.parse(bytes.NewBuffer(tt.osRelease))
-			if dist != nil {
-				t.Fatalf("expected nil dist got %v", dist)
-			}
-			dist = scanner.parse(bytes.NewBuffer(tt.lsbRelease))
-			if dist != nil {
-				t.Fatalf("expected nil dist got %v", dist)
 			}
 		})
 	}
