9ff1b19c908ec01d1af577248a701f0ab8d2911d
Ales Raszka <araszka@redhat.com>
2020-05-04 14:34:50 +0200

    Include repository in json output
    Now when we use repositories in vulnerabilities we should also expose it
    in API.

--- repository.go
+++ repository.go
@@ -2,8 +2,8 @@ package claircore
 
 // Repository is a package repository
 type Repository struct {
-	ID   string `json:"id"`
-	Name string `json:"name"`
-	Key  string `json:"key"`
-	URI  string `json:"uri"`
+	ID   string `json:"id,omitempty"`
+	Name string `json:"name,omitempty"`
+	Key  string `json:"key,omitempty"`
+	URI  string `json:"uri,omitempty"`
 }
