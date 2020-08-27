104c5f3290d04bb361eb65ef172a47b8183d02bd
Hank Donnay <hdonnay@redhat.com>
2020-08-27 16:10:40 -0400

    libvuln: add OfflineImport function
    This function is needed to access some internal packages when importing
    updates from an artifact produced by the offline updater.
    
    Signed-off-by: Hank Donnay <hdonnay@redhat.com>

--- libvuln/updates.go
+++ libvuln/updates.go
@@ -1,6 +1,7 @@
 package libvuln
 
 import (
+	"compress/gzip"
 	"context"
 	"io"
 	"net/http"
@@ -9,6 +10,8 @@ import (
 	"github.com/rs/zerolog"
 
 	"github.com/quay/claircore/internal/updater"
+	"github.com/quay/claircore/internal/vulnstore/jsonblob"
+	"github.com/quay/claircore/internal/vulnstore/postgres"
 	"github.com/quay/claircore/libvuln/driver"
 )
 
@@ -32,6 +35,63 @@ func NewOfflineUpdater(config map[string]driver.ConfigUnmarshaler, filter func(s
 		},
 		config: config,
 	}, nil
+}
+
+// OfflineImport takes the format written into the io.Writer provided to
+// NewOfflineUpdater and imports the contents into the provided pgxpool.Pool.
+func OfflineImport(ctx context.Context, pool *pgxpool.Pool, in io.Reader) error {
+	// BUG(hank) The OfflineImport function is a wart, needed to work around
+	// some package namespacing issues. It should get refactored if claircore
+	// gets merged into clair.
+	log := zerolog.Ctx(ctx).With().
+		Str("component", "libvuln/OfflineImporter").
+		Logger()
+	ctx = log.WithContext(ctx)
+
+	gz, err := gzip.NewReader(in)
+	if err != nil {
+		return err
+	}
+	defer gz.Close()
+
+	s := postgres.NewVulnStore(pool)
+	l, err := jsonblob.Load(ctx, gz)
+	if err != nil {
+		return err
+	}
+
+	ops, err := s.GetUpdateOperations(ctx)
+	if err != nil {
+		return err
+	}
+
+Update:
+	for l.Next() {
+		e := l.Entry()
+		for _, op := range ops[e.Updater] {
+			// This only helps if updaters don't keep something that
+			// changes in the fingerprint.
+			if op.Fingerprint == e.Fingerprint {
+				log.Info().
+					Str("updater", e.Updater).
+					Msg("fingerprint match, skipping")
+				continue Update
+			}
+		}
+		ref, err := s.UpdateVulnerabilities(ctx, e.Updater, e.Fingerprint, e.Vuln)
+		if err != nil {
+			return err
+		}
+		log.Info().
+			Str("updater", e.Updater).
+			Str("ref", ref.String()).
+			Int("count", len(e.Vuln)).
+			Msg("update imported")
+	}
+	if err := l.Err(); err != nil {
+		return err
+	}
+	return nil
 }
 
 type UpdateDriver struct {
