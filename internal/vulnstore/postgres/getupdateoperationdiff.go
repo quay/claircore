7a8f19c4c0a84c3ec6dd3dd46d2e35fa32dd8fbf
ldelossa <ldelossa@localhost.localdomain>
2020-09-23 16:19:39 -0400

    vulnstore: limit diffs
    this commit puts a limit on the number of returned vulnerabilities when
    diffing two update operations.
    
    Signed-off-by: ldelossa <ldelossa@localhost.localdomain>

--- internal/vulnstore/postgres/getupdateoperationdiff.go
+++ internal/vulnstore/postgres/getupdateoperationdiff.go
@@ -50,7 +50,8 @@ func getUpdateDiff(ctx context.Context, pool *pgxpool.Pool, prev, cur uuid.UUID)
 		vuln.id IN (
 			SELECT vuln AS id FROM uo_vuln JOIN lhs ON (uo_vuln.uo = lhs.id)
 			EXCEPT ALL
-			SELECT vuln AS id FROM uo_vuln JOIN rhs ON (uo_vuln.uo = rhs.id));`
+			SELECT vuln AS id FROM uo_vuln JOIN rhs ON (uo_vuln.uo = rhs.id))
+	LIMIT 500;`
 
 	if cur == uuid.Nil {
 		return nil, errors.New("nil uuid is invalid as \"current\" endpoint")
