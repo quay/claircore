ad1edc9aa3aa7540af1e4aaf9fd94fe18de03395
hdonnay <hdonnay@users.noreply.github.com>
2021-01-15 18:32:38 +0000

    Automated deployment: Fri Jan 15 18:32:38 UTC 2021 3a4e3d3e053cd3856795565e1b674e2ba4b03900

--- logger_test.go
+++ logger_test.go
@@ -4,31 +4,25 @@ import (
 	"context"
 	"time"
 
-	"github.com/rs/zerolog"
+	"github.com/quay/zlog"
+	"go.opentelemetry.io/otel/baggage"
+	"go.opentelemetry.io/otel/label"
 )
 
 // Example_logger is an example annotated for inclusion in the prose
 // documentation.
 func Example_logger() {
 	ctx := context.Background()
-	// ANCHOR: logger
-	log := zerolog.Ctx(ctx).With().
-		// ANCHOR_END: logger
-		// ANCHOR: kvs
-		Str("component", "Example.Logger").
-		// ANCHOR_END: kvs
-		// ANCHOR: newlogger
-		Logger()
-		// ANCHOR_END: newlogger
-	// ANCHOR: context
-	ctx = log.WithContext(ctx)
-	// ANCHOR_END: context
+	// ANCHOR: kvs
+	ctx = baggage.ContextWithValues(ctx,
+		label.String("component", "Example.Logger"))
+	// ANCHOR_END: kvs
 
 	// ANCHOR: bad_example
-	log.Info().Msgf("done at: %v", time.Now())
+	zlog.Info(ctx).Msgf("done at: %v", time.Now())
 	// ANCHOR_END: bad_example
 	// ANCHOR: good_example
-	log.Info().
+	zlog.Info(ctx).
 		Time("time", time.Now()).
 		Msgf("done")
 	// ANCHOR_END: good_example
