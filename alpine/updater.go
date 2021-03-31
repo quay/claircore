f92e1bea4f163be841fbc459c4b909a0512f1d04
Hank Donnay <hdonnay@redhat.com>
2021-03-31 10:31:05 -0500

    alpine: implement driver.Configurable
    Signed-off-by: Hank Donnay <hdonnay@redhat.com>

--- alpine/updater.go
+++ alpine/updater.go
@@ -1,8 +1,11 @@
 package alpine
 
 import (
+	"context"
 	"fmt"
 	"net/http"
+
+	"github.com/quay/zlog"
 
 	"github.com/quay/claircore/libvuln/driver"
 )
@@ -23,12 +26,18 @@ type Updater struct {
 	url     string
 }
 
-var _ driver.Updater = (*Updater)(nil)
+var (
+	_ driver.Updater      = (*Updater)(nil)
+	_ driver.Configurable = (*Updater)(nil)
+)
 
 // Option configures the provided Updater
 type Option func(*Updater) error
 
 // WithURL overrides the default URL to fetch an OVAL database.
+//
+// The default is derived from the Release and Repo arguments passed to
+// NewUpdater.
 func WithURL(url string) Option {
 	return func(u *Updater) error {
 		u.url = url
@@ -47,7 +56,7 @@ func WithClient(c *http.Client) Option {
 // NewUpdater returns an updater configured according to the provided Options.
 func NewUpdater(release Release, repo Repo, opts ...Option) (*Updater, error) {
 	u := &Updater{
-		client:  http.DefaultClient,
+		client:  http.DefaultClient, // TODO(hank) Remove DefaultClient
 		release: release,
 		repo:    repo,
 		url:     DBUrl(release, repo),
@@ -64,4 +73,30 @@ func NewUpdater(release Release, repo Repo, opts ...Option) (*Updater, error) {
 
 func (u *Updater) Name() string {
 	return fmt.Sprintf("alpine-%s-%s-updater", u.repo, u.release)
+}
+
+// UpdaterConfig is the configuration accepted by Alpine updaters.
+//
+// By convention, this should be in a map called
+// "alpine-${REPO}-${RELEASE}-updater". For example,
+// "alpine-main-v3.12-updater".
+type UpdaterConfig struct {
+	URL string `json:"url" yaml:"url"`
+}
+
+// Configure implements driver.Configurable.
+func (u *Updater) Configure(ctx context.Context, f driver.ConfigUnmarshaler, c *http.Client) error {
+	var cfg UpdaterConfig
+	if err := f(&cfg); err != nil {
+		return err
+	}
+	if cfg.URL != "" {
+		u.url = cfg.URL
+		zlog.Info(ctx).
+			Str("component", "alpine/Updater.Configure").
+			Str("updater", u.Name()).
+			Msg("configured url")
+	}
+	u.client = c
+	return nil
 }
