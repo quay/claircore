dbd56bbb408d05e87df9dcc6e7b463bf2c162958
Hank Donnay <hdonnay@redhat.com>
2021-07-01 14:41:23 -0500

    jsonblob: implement enricher methods
    Signed-off-by: Hank Donnay <hdonnay@redhat.com>

--- libvuln/jsonblob/jsonblob.go
+++ libvuln/jsonblob/jsonblob.go
@@ -23,6 +23,7 @@ func New() (*Store, error) {
 	s := Store{}
 	s.ops = make(map[string][]driver.UpdateOperation)
 	s.entry = make(map[uuid.UUID]*Entry)
+	s.latest = make(map[driver.UpdateKind]uuid.UUID)
 	return &s, nil
 }
 
@@ -31,7 +32,7 @@ type Store struct {
 	sync.RWMutex
 	entry  map[uuid.UUID]*Entry
 	ops    map[string][]driver.UpdateOperation
-	latest uuid.UUID
+	latest map[driver.UpdateKind]uuid.UUID
 }
 
 // Load reads in all the records serialized in the provided Reader.
@@ -126,7 +127,8 @@ func (s *Store) Store(w io.Writer) error {
 // later date.
 type Entry struct {
 	CommonEntry
-	Vuln []*claircore.Vulnerability
+	Vuln       []*claircore.Vulnerability
+	Enrichment []driver.EnrichmentRecord
 }
 
 // CommonEntry is an embedded type that's shared between the "normal" Entry type
@@ -141,8 +143,10 @@ type CommonEntry struct {
 // slice and adding a uuid for grouping back into an Entry upon read.
 type diskEntry struct {
 	CommonEntry
-	Ref  uuid.UUID
-	Vuln *claircore.Vulnerability
+	Ref        uuid.UUID
+	Vuln       *claircore.Vulnerability
+	Enrichment *driver.EnrichmentRecord
+	Kind       driver.UpdateKind
 }
 
 // Entries returns a map containing all the Entries stored by calls to
@@ -167,7 +171,7 @@ func (s *Store) UpdateVulnerabilities(_ context.Context, updater string, fingerp
 	ref := uuid.New() // God help you if this wasn't unique.
 	s.Lock()
 	defer s.Unlock()
-	s.latest = ref
+	s.latest[driver.VulnerabilityKind] = ref
 	s.entry[ref] = &e
 	s.ops[updater] = append([]driver.UpdateOperation{{
 		Ref:         ref,
@@ -180,11 +184,29 @@ func (s *Store) UpdateVulnerabilities(_ context.Context, updater string, fingerp
 }
 
 // Copyops assumes all locks are taken care of.
-func (s *Store) copyops() map[string][]driver.UpdateOperation {
+func (s *Store) copyops(ty driver.UpdateKind, us ...string) map[string][]driver.UpdateOperation {
+	ns := make(map[string]struct{})
+	for _, n := range us {
+		ns[n] = struct{}{}
+	}
 	m := make(map[string][]driver.UpdateOperation, len(s.ops))
 	for k, v := range s.ops {
+		// If we were passed a set of names and this wasn't in it, pass.
+		// If we weren't passed a set of names, do the copy for everything.
+		if _, ok := ns[k]; len(ns) != 0 && !ok {
+			continue
+		}
 		n := make([]driver.UpdateOperation, len(v))
 		copy(n, v)
+		// Filter our copy by type, in place.
+		i := 0
+		for _, op := range n {
+			if op.Kind == ty {
+				n[i] = op
+				i++
+			}
+		}
+		n = n[:i]
 		sort.Slice(n, func(i, j int) bool { return n[i].Date.Before(n[j].Date) })
 		m[k] = n
 	}
@@ -197,26 +219,26 @@ func (s *Store) copyops() map[string][]driver.UpdateOperation {
 // The returned map is keyed by Updater implementation's unique names.
 //
 // If no updaters are specified, all UpdateOperations are returned.
-func (s *Store) GetUpdateOperations(context.Context, driver.UpdateKind, ...string) (map[string][]driver.UpdateOperation, error) {
+func (s *Store) GetUpdateOperations(_ context.Context, k driver.UpdateKind, us ...string) (map[string][]driver.UpdateOperation, error) {
 	s.RLock()
 	defer s.RUnlock()
-	return s.copyops(), nil
+	return s.copyops(k, us...), nil
 }
 
 // GetLatestUpdateRefs reports the latest update reference for every known
 // updater.
-func (s *Store) GetLatestUpdateRefs(context.Context, driver.UpdateKind) (map[string][]driver.UpdateOperation, error) {
+func (s *Store) GetLatestUpdateRefs(_ context.Context, k driver.UpdateKind) (map[string][]driver.UpdateOperation, error) {
 	s.RLock()
 	defer s.RUnlock()
-	return s.copyops(), nil
+	return s.copyops(k), nil
 }
 
 // GetLatestUpdateRef reports the latest update reference of any known
 // updater.
-func (s *Store) GetLatestUpdateRef(context.Context, driver.UpdateKind) (uuid.UUID, error) {
+func (s *Store) GetLatestUpdateRef(_ context.Context, k driver.UpdateKind) (uuid.UUID, error) {
 	s.RLock()
 	defer s.RUnlock()
-	return s.latest, nil
+	return s.latest[k], nil
 }
 
 // DeleteUpdateOperations is unimplemented.
@@ -239,4 +261,30 @@ func (s *Store) Initialized(context.Context) (bool, error) {
 // GC is unimplemented.
 func (s *Store) GC(_ context.Context, _ int) (int64, error) {
 	return 0, nil
+}
+
+// UpdateEnrichments creates a new EnrichmentUpdateOperation, inserts the provided
+// EnrichmentRecord(s), and ensures enrichments from previous updates are not
+// queries by clients.
+func (s *Store) UpdateEnrichments(ctx context.Context, kind string, fp driver.Fingerprint, es []driver.EnrichmentRecord) (uuid.UUID, error) {
+	now := time.Now()
+	e := Entry{
+		Enrichment: es,
+	}
+	e.Date = now
+	e.Updater = kind
+	e.Fingerprint = fp
+	ref := uuid.New() // God help you if this wasn't unique.
+	s.Lock()
+	defer s.Unlock()
+	s.latest[driver.EnrichmentKind] = ref
+	s.entry[ref] = &e
+	s.ops[kind] = append([]driver.UpdateOperation{{
+		Ref:         ref,
+		Date:        now,
+		Fingerprint: fp,
+		Updater:     kind,
+		Kind:        driver.EnrichmentKind,
+	}}, s.ops[kind]...)
+	return ref, nil
 }
