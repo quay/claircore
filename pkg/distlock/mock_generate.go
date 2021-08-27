56166c3c0b3853b46df680cea781c33d171ca8f2
Hank Donnay <hdonnay@redhat.com>
2021-08-27 12:54:44 -0500

    distlock: remove package
    This is unused, now that ctxlock is integrated.
    
    Signed-off-by: Hank Donnay <hdonnay@redhat.com>

--- pkg/distlock/mock_generate.go
+++
@@ -1,3 +0,0 @@
-package distlock
-
-//go:generate mockgen -package=distlock -self_package=github.com/quay/claircore/pkg/distlock -destination=./locker_mock.go github.com/quay/claircore/pkg/distlock Locker
