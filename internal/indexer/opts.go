5a27c1f4b078492f922144715b8d57cebd135b68
Hank Donnay <hdonnay@redhat.com>
2021-08-27 12:53:14 -0500

    indexer: remove ScanLock member
    This also shuffles member layout to minimize padding.
    
    Signed-off-by: Hank Donnay <hdonnay@redhat.com>

--- internal/indexer/opts.go
+++ internal/indexer/opts.go
@@ -2,21 +2,18 @@ package indexer
 
 import (
 	"net/http"
-
-	"github.com/quay/claircore/pkg/distlock"
 )
 
 // Opts are options to instantiate a indexer
 type Opts struct {
-	Store         Store
-	ScanLock      distlock.Locker
-	LayerScanner  LayerScanner
-	Fetcher       Fetcher
-	Ecosystems    []*Ecosystem
-	Vscnrs        VersionedScanners
-	Airgap        bool
 	Client        *http.Client
 	ScannerConfig struct {
 		Package, Dist, Repo map[string]func(interface{}) error
 	}
+	Store        Store
+	LayerScanner LayerScanner
+	Fetcher      Fetcher
+	Ecosystems   []*Ecosystem
+	Vscnrs       VersionedScanners
+	Airgap       bool
 }
