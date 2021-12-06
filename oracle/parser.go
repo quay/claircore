43d5cba1cb9a859c1a01624a0bf7ed48d70e86c3
Hank Donnay <hdonnay@redhat.com>
2021-12-06 11:26:50 -0600

    oracle: use CharsetReader
    Signed-off-by: Hank Donnay <hdonnay@redhat.com>

--- oracle/parser.go
+++ oracle/parser.go
@@ -12,6 +12,7 @@ import (
 	"go.opentelemetry.io/otel/label"
 
 	"github.com/quay/claircore"
+	"github.com/quay/claircore/internal/xmlutil"
 	"github.com/quay/claircore/libvuln/driver"
 	"github.com/quay/claircore/pkg/ovalutil"
 )
@@ -39,7 +40,9 @@ func (u *Updater) Parse(ctx context.Context, r io.ReadCloser) ([]*claircore.Vuln
 	zlog.Info(ctx).Msg("starting parse")
 	defer r.Close()
 	root := oval.Root{}
-	if err := xml.NewDecoder(r).Decode(&root); err != nil {
+	dec := xml.NewDecoder(r)
+	dec.CharsetReader = xmlutil.CharsetReader
+	if err := dec.Decode(&root); err != nil {
 		return nil, fmt.Errorf("oracle: unable to decode OVAL document: %w", err)
 	}
 	zlog.Debug(ctx).Msg("xml decoded")
