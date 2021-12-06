605ee512be4a906950fdce1586ee0343858ad47c
Hank Donnay <hdonnay@redhat.com>
2021-12-06 11:27:56 -0600

    rhel: use CharsetReader
    Signed-off-by: Hank Donnay <hdonnay@redhat.com>

--- rhel/parser.go
+++ rhel/parser.go
@@ -12,6 +12,7 @@ import (
 	"go.opentelemetry.io/otel/label"
 
 	"github.com/quay/claircore"
+	"github.com/quay/claircore/internal/xmlutil"
 	"github.com/quay/claircore/pkg/cpe"
 	"github.com/quay/claircore/pkg/ovalutil"
 )
@@ -22,7 +23,9 @@ func (u *Updater) Parse(ctx context.Context, r io.ReadCloser) ([]*claircore.Vuln
 	zlog.Info(ctx).Msg("starting parse")
 	defer r.Close()
 	root := oval.Root{}
-	if err := xml.NewDecoder(r).Decode(&root); err != nil {
+	dec := xml.NewDecoder(r)
+	dec.CharsetReader = xmlutil.CharsetReader
+	if err := dec.Decode(&root); err != nil {
 		return nil, fmt.Errorf("rhel: unable to decode OVAL document: %w", err)
 	}
 	zlog.Debug(ctx).Msg("xml decoded")
