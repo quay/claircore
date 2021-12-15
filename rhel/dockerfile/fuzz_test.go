397136fb7ff959dfb994f67b3c0df1f760ec33c7
Hank Donnay <hdonnay@redhat.com>
2021-12-15 15:22:01 -0600

    dockerfile: add some fuzz testing
    This is what I was trying to write when I noticed the comment issue.
    
    Signed-off-by: Hank Donnay <hdonnay@redhat.com>

---
+++ rhel/dockerfile/fuzz_test.go
@@ -0,0 +1,28 @@
+//go:build go1.18
+// +build go1.18
+
+package dockerfile
+
+import (
+	"bytes"
+	"testing"
+)
+
+func FuzzLex(f *testing.F) {
+	f.Fuzz(func(t *testing.T, b []byte) {
+		l := newLexer()
+		l.Reset(bytes.NewReader(b))
+		for {
+			switch i := l.Next(); i.kind {
+			case itemEOF:
+				return
+			case itemError:
+				t.Fatal(i.val)
+			default:
+				t.Logf("%v:\t%s", i.kind, i.val)
+			}
+		}
+	})
+}
+
+//go:generate sh -c "file2fuzz -o testdata/fuzz/FuzzLex $(ls -1 testdata/Dockerfile* | grep -v [.]want)"
