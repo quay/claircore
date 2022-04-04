e452f646c6d92b3a0737e92f434d7552960f1eec
Hank Donnay <hdonnay@redhat.com>
2022-04-04 17:00:09 -0500

    postgres: use bisect tool to generate test fixtures
    Signed-off-by: Hank Donnay <hdonnay@redhat.com>

--- datastore/postgres/generate.go
+++ datastore/postgres/generate.go
@@ -4,4 +4,5 @@ package postgres
 //
 // Splitting it is not allowed.
 
-//go:generate go run ../../../cmd/cctool report -dump -index-fmt "testdata/{{zipSan .}}.index.json" -report-fmt "testdata/{{zipSan .}}.report.json" -manifest-fmt /dev/null docker.io/library/amazonlinux:1 docker.io/library/debian:10 docker.io/library/debian:9 docker.io/library/debian:8 docker.io/mitmproxy/mitmproxy:4.0.1 docker.io/library/ubuntu:16.04 docker.io/library/ubuntu:18.04 docker.io/library/ubuntu:19.10 docker.io/library/ubuntu:20.04 registry.access.redhat.com/ubi8/ubi
+//go:generate -command mktestdata go run ../../../test/bisect -dump-index "testdata/{{.}}.index.json" -dump-report "testdata/{{.}}.report.json"
+//go:generate mktestdata docker.io/library/amazonlinux:1 docker.io/library/debian:10 docker.io/library/debian:9 docker.io/library/debian:8 docker.io/mitmproxy/mitmproxy:4.0.1 docker.io/library/ubuntu:16.04 docker.io/library/ubuntu:18.04 docker.io/library/ubuntu:19.10 docker.io/library/ubuntu:20.04 registry.access.redhat.com/ubi8/ubi
