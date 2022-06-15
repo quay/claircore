cef8829f790390d03d56d982ce128cb65883402a
Hank Donnay <hdonnay@redhat.com>
2022-06-15 09:33:55 -0500

    alpine: remove disused method
    Signed-off-by: Hank Donnay <hdonnay@redhat.com>

--- alpine/distributionscanner.go
+++ alpine/distributionscanner.go
@@ -29,74 +29,6 @@ const (
 	scannerKind    = "distribution"
 )
 
-type alpineRegex struct {
-	dist   *claircore.Distribution
-	regexp *regexp.Regexp
-}
-
-// the following regexps will match the PrettyName in the os-release file
-// ex: "Alpine Linux v3.3"
-// and the issue string in the issue file
-// ex: "Welcome to Alpine Linux 3.3"
-var alpineRegexes = []alpineRegex{
-	{
-		dist:   alpine3_16Dist,
-		regexp: regexp.MustCompile(`Alpine Linux v?3\.16`),
-	},
-	{
-		dist:   alpine3_15Dist,
-		regexp: regexp.MustCompile(`Alpine Linux v?3\.15`),
-	},
-	{
-		dist:   alpine3_14Dist,
-		regexp: regexp.MustCompile(`Alpine Linux v?3\.14`),
-	},
-	{
-		dist:   alpine3_13Dist,
-		regexp: regexp.MustCompile(`Alpine Linux v?3\.13`),
-	},
-	{
-		dist:   alpine3_12Dist,
-		regexp: regexp.MustCompile(`Alpine Linux v?3\.12`),
-	},
-	{
-		dist:   alpine3_11Dist,
-		regexp: regexp.MustCompile(`Alpine Linux v?3\.11`),
-	},
-	{
-		dist:   alpine3_10Dist,
-		regexp: regexp.MustCompile(`Alpine Linux v?3\.10`),
-	},
-	{
-		dist:   alpine3_9Dist,
-		regexp: regexp.MustCompile(`Alpine Linux v?3\.9`),
-	},
-	{
-		dist:   alpine3_8Dist,
-		regexp: regexp.MustCompile(`Alpine Linux v?3\.8`),
-	},
-	{
-		dist:   alpine3_7Dist,
-		regexp: regexp.MustCompile(`Alpine Linux v?3\.7`),
-	},
-	{
-		dist:   alpine3_6Dist,
-		regexp: regexp.MustCompile(`Alpine Linux v?3\.6`),
-	},
-	{
-		dist:   alpine3_5Dist,
-		regexp: regexp.MustCompile(`Alpine Linux v?3\.5`),
-	},
-	{
-		dist:   alpine3_4Dist,
-		regexp: regexp.MustCompile(`Alpine Linux v?3\.4`),
-	},
-	{
-		dist:   alpine3_3Dist,
-		regexp: regexp.MustCompile(`Alpine Linux v?3\.3`),
-	},
-}
-
 const (
 	osReleasePath = `etc/os-release`
 	issuePath     = `etc/issue`
@@ -105,6 +37,8 @@ const (
 var (
 	_ indexer.DistributionScanner = (*DistributionScanner)(nil)
 	_ indexer.VersionedScanner    = (*DistributionScanner)(nil)
+
+	issueRegexp = regexp.MustCompile(`Alpine Linux ([[:digit:]]+\.[[:digit:]]+)`)
 )
 
 // DistributionScanner attempts to discover if a layer
@@ -125,11 +59,11 @@ func (*DistributionScanner) Kind() string { return scannerKind }
 //
 // If neither file is found a (nil, nil) is returned.
 // If the files are found but all regexp fail to match an empty slice is returned.
-func (ds *DistributionScanner) Scan(ctx context.Context, l *claircore.Layer) ([]*claircore.Distribution, error) {
+func (s *DistributionScanner) Scan(ctx context.Context, l *claircore.Layer) ([]*claircore.Distribution, error) {
 	defer trace.StartRegion(ctx, "Scanner.Scan").End()
 	ctx = zlog.ContextWithValues(ctx,
 		"component", "alpine/DistributionScanner.Scan",
-		"version", ds.Version(),
+		"version", s.Version(),
 		"layer", l.Hash.String())
 	zlog.Debug(ctx).Msg("start")
 	defer zlog.Debug(ctx).Msg("done")
@@ -142,10 +76,10 @@ func (ds *DistributionScanner) Scan(ctx context.Context, l *claircore.Layer) ([]
 	if err != nil {
 		return nil, err
 	}
-	return ds.scanFs(ctx, sys)
+	return s.scanFs(ctx, sys)
 }
 
-func (s *DistributionScanner) scanFs(ctx context.Context, sys fs.FS) (d []*claircore.Distribution, err error) {
+func (*DistributionScanner) scanFs(ctx context.Context, sys fs.FS) (d []*claircore.Distribution, err error) {
 	// Use weirdo goto construction to pick the first instance.
 	var b []byte
 
@@ -217,19 +151,4 @@ func (s *DistributionScanner) scanFs(ctx context.Context, sys fs.FS) (d []*clair
 
 Done:
 	return d, nil
-}
-
-var issueRegexp = regexp.MustCompile(`Alpine Linux ([[:digit:]]+\.[[:digit:]]+)`)
-
-// parse attempts to match all Alpine release regexp and returns the associated
-// distribution if it exists.
-//
-// separated into its own method to aid testing.
-func (ds *DistributionScanner) parse(buff *bytes.Buffer) *claircore.Distribution {
-	for _, ur := range alpineRegexes {
-		if ur.regexp.Match(buff.Bytes()) {
-			return ur.dist
-		}
-	}
-	return nil
 }
--- alpine/distributionscanner_test.go
+++ alpine/distributionscanner_test.go
@@ -1,125 +1,17 @@
 package alpine
 
 import (
-	"bytes"
 	"context"
 	"encoding/json"
-	"io"
 	"io/fs"
 	"os"
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
-	"github.com/quay/claircore"
 	"github.com/quay/zlog"
+
+	"github.com/quay/claircore"
 )
-
-type distTestcase struct {
-	Release
-	OSRelease string
-	Issue     string
-}
-
-func TestDistributionScanner(t *testing.T) {
-	table := []distTestcase{
-		{
-			Release:   V3_3,
-			OSRelease: mustRead(t, `testdata/3.3/etc/os-release`),
-			Issue:     mustRead(t, `testdata/3.3/etc/issue`),
-		},
-		{
-			Release:   V3_4,
-			OSRelease: mustRead(t, `testdata/3.4/etc/os-release`),
-			Issue:     mustRead(t, `testdata/3.4/etc/issue`),
-		},
-		{
-			Release:   V3_5,
-			OSRelease: mustRead(t, `testdata/3.5/etc/os-release`),
-			Issue:     mustRead(t, `testdata/3.5/etc/issue`),
-		},
-		{
-			Release:   V3_6,
-			OSRelease: mustRead(t, `testdata/3.6/etc/os-release`),
-			Issue:     mustRead(t, `testdata/3.6/etc/issue`),
-		},
-		{
-			Release:   V3_7,
-			OSRelease: mustRead(t, `testdata/3.7/etc/os-release`),
-			Issue:     mustRead(t, `testdata/3.7/etc/issue`),
-		},
-		{
-			Release:   V3_8,
-			OSRelease: mustRead(t, `testdata/3.8/etc/os-release`),
-			Issue:     mustRead(t, `testdata/3.8/etc/issue`),
-		},
-		{
-			Release:   V3_9,
-			OSRelease: mustRead(t, `testdata/3.9/etc/os-release`),
-			Issue:     mustRead(t, `testdata/3.9/etc/issue`),
-		},
-		{
-			Release:   V3_10,
-			OSRelease: mustRead(t, `testdata/3.10/etc/os-release`),
-			Issue:     mustRead(t, `testdata/3.10/etc/issue`),
-		},
-		{
-			Release:   V3_11,
-			OSRelease: mustRead(t, `testdata/3.11/etc/os-release`),
-			Issue:     mustRead(t, `testdata/3.11/etc/issue`),
-		},
-		{
-			Release:   V3_12,
-			OSRelease: mustRead(t, `testdata/3.12/etc/os-release`),
-			Issue:     mustRead(t, `testdata/3.12/etc/issue`),
-		},
-		{
-			Release:   V3_13,
-			OSRelease: mustRead(t, `testdata/3.13/etc/os-release`),
-			Issue:     mustRead(t, `testdata/3.13/etc/issue`),
-		},
-		{
-			Release:   V3_14,
-			OSRelease: mustRead(t, `testdata/3.14/etc/os-release`),
-			Issue:     mustRead(t, `testdata/3.14/etc/issue`),
-		},
-		{
-			Release:   V3_15,
-			OSRelease: mustRead(t, `testdata/3.15/etc/os-release`),
-			Issue:     mustRead(t, `testdata/3.15/etc/issue`),
-		},
-		{
-			Release:   V3_16,
-			OSRelease: mustRead(t, `testdata/3.16/etc/os-release`),
-			Issue:     mustRead(t, `testdata/3.16/etc/issue`),
-		},
-	}
-	for _, tt := range table {
-		t.Run(string(tt.Release), func(t *testing.T) {
-			scanner := DistributionScanner{}
-			dist := scanner.parse(bytes.NewBufferString(tt.OSRelease))
-			if got, want := dist, releaseToDist(tt.Release); !cmp.Equal(got, want) {
-				t.Error(cmp.Diff(got, want))
-			}
-			dist = scanner.parse(bytes.NewBufferString(tt.Issue))
-			if got, want := dist, releaseToDist(tt.Release); !cmp.Equal(got, want) {
-				t.Error(cmp.Diff(got, want))
-			}
-		})
-	}
-}
-
-func mustRead(t *testing.T, p string) string {
-	f, err := os.Open(p)
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer f.Close()
-	b, err := io.ReadAll(f)
-	if err != nil {
-		t.Fatal(err)
-	}
-	return string(b)
-}
 
 func TestScanFs(t *testing.T) {
 	ctx := context.Background()
