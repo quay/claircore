adae02acb0d23bfab6c8b94cf7fcfe6c6f7286ba
Hank Donnay <hdonnay@redhat.com>
2023-01-20 11:41:26 -0600

    postgres: Connect errors
    Also removes the hard-coded 30 max connections.
    
    Signed-off-by: Hank Donnay <hdonnay@redhat.com>

--- datastore/postgres/connect.go
+++ datastore/postgres/connect.go
@@ -2,23 +2,32 @@ package postgres
 
 import (
 	"context"
-	"fmt"
 
 	"github.com/jackc/pgx/v4/pgxpool"
 	"github.com/prometheus/client_golang/prometheus"
-	"github.com/quay/claircore/pkg/poolstats"
 	"github.com/quay/zlog"
+
+	"github.com/quay/claircore"
+	"github.com/quay/claircore/pkg/poolstats"
 )
 
-// Connect initialize a postgres pgxpool.Pool based on the connection string
+// Connect initializes a [pgxpool.Pool] based on the connection string.
 func Connect(ctx context.Context, connString string, applicationName string) (*pgxpool.Pool, error) {
-	// we are going to use pgx for more control over connection pool and
-	// and a cleaner api around bulk inserts
+	const op = `datastore/postgres/Connect`
 	cfg, err := pgxpool.ParseConfig(connString)
 	if err != nil {
-		return nil, fmt.Errorf("failed to parse ConnString: %v", err)
+		return nil, &claircore.Error{
+			Op:      op,
+			Kind:    claircore.ErrInvalid,
+			Message: "failed to parse connection string",
+			Inner: &claircore.Error{
+				// Permanent because the same connection string should always
+				// yield an error.
+				Kind:  claircore.ErrPermanent,
+				Inner: err,
+			},
+		}
 	}
-	cfg.MaxConns = 30
 	const appnameKey = `application_name`
 	params := cfg.ConnConfig.RuntimeParams
 	if _, ok := params[appnameKey]; !ok {
@@ -27,7 +36,12 @@ func Connect(ctx context.Context, connString string, applicationName string) (*p
 
 	pool, err := pgxpool.ConnectConfig(ctx, cfg)
 	if err != nil {
-		return nil, fmt.Errorf("failed to create ConnPool: %v", err)
+		return nil, &claircore.Error{
+			Op:      op,
+			Kind:    claircore.ErrPrecondition,
+			Message: "failed to create connection pool",
+			Inner:   err,
+		}
 	}
 
 	if err := prometheus.Register(poolstats.NewCollector(pool, applicationName)); err != nil {
