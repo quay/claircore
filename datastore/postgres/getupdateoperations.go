a44511a3894cd0cb855849291c3a89bff109b3f9
Hank Donnay <hdonnay@redhat.com>
2023-02-09 12:45:05 -0600

    postgres: GetUpdateRef/GetUpdateOp/etc errors
    Signed-off-by: Hank Donnay <hdonnay@redhat.com>

--- datastore/postgres/getupdateoperations.go
+++ datastore/postgres/getupdateoperations.go
@@ -4,15 +4,14 @@ import (
 	"context"
 	"errors"
 	"fmt"
-	"time"
 
 	"github.com/google/uuid"
 	"github.com/jackc/pgx/v4"
-	"github.com/jackc/pgx/v4/pgxpool"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promauto"
 	"github.com/quay/zlog"
 
+	"github.com/quay/claircore"
 	"github.com/quay/claircore/libvuln/driver"
 )
 
@@ -76,11 +75,12 @@ var (
 // GetLatestUpdateRef implements driver.Updater.
 func (s *MatcherStore) GetLatestUpdateRef(ctx context.Context, kind driver.UpdateKind) (uuid.UUID, error) {
 	const (
+		op                 = `datastore/postgres/MatcherStore.GetLatestUpdateRef`
 		query              = `SELECT ref FROM update_operation ORDER BY id USING > LIMIT 1;`
 		queryEnrichment    = `SELECT ref FROM update_operation WHERE kind = 'enrichment' ORDER BY id USING > LIMIT 1;`
 		queryVulnerability = `SELECT ref FROM update_operation WHERE kind = 'vulnerability' ORDER BY id USING > LIMIT 1;`
 	)
-	ctx = zlog.ContextWithValues(ctx, "component", "internal/vulnstore/postgres/getLatestRef")
+	ctx = zlog.ContextWithValues(ctx, "component", op)
 
 	var q string
 	var label string
@@ -96,22 +96,28 @@ func (s *MatcherStore) GetLatestUpdateRef(ctx context.Context, kind driver.Updat
 		label = "query_vulnerability"
 	}
 
+	defer prometheus.NewTimer(getLatestUpdateRefDuration.WithLabelValues(label)).ObserveDuration()
+	defer getLatestUpdateRefCounter.WithLabelValues(label).Inc()
 	var ref uuid.UUID
-	start := time.Now()
 	if err := s.pool.QueryRow(ctx, q).Scan(&ref); err != nil {
-		return uuid.Nil, err
+		return uuid.Nil, &claircore.Error{
+			Op:      op,
+			Kind:    claircore.ErrInternal,
+			Message: "error querying latest UpdateOperation",
+			Inner:   err,
+		}
 	}
-	getLatestUpdateRefCounter.WithLabelValues(label).Add(1)
-	getLatestUpdateRefDuration.WithLabelValues(label).Observe(time.Since(start).Seconds())
 	return ref, nil
 }
 
 func (s *MatcherStore) GetLatestUpdateRefs(ctx context.Context, kind driver.UpdateKind) (map[string][]driver.UpdateOperation, error) {
 	const (
+		op                 = `datastore/postgres/MatcherStore.GetLatestUpdateRefs`
 		query              = `SELECT DISTINCT ON (updater) updater, ref, fingerprint, date FROM update_operation ORDER BY updater, id USING >;`
 		queryEnrichment    = `SELECT DISTINCT ON (updater) updater, ref, fingerprint, date FROM update_operation WHERE kind = 'enrichment' ORDER BY updater, id USING >;`
 		queryVulnerability = `SELECT DISTINCT ON (updater) updater, ref, fingerprint, date FROM update_operation WHERE kind = 'vulnerability' ORDER BY updater, id USING >;`
 	)
+	ctx = zlog.ContextWithValues(ctx, "component", op)
 
 	var q string
 	var label string
@@ -127,73 +133,62 @@ func (s *MatcherStore) GetLatestUpdateRefs(ctx context.Context, kind driver.Upda
 		label = "query_vulnerability"
 	}
 
-	start := time.Now()
-
-	rows, err := s.pool.Query(ctx, q)
-	if err != nil {
-		return nil, err
-	}
-
-	getLatestRefsCounter.WithLabelValues(label).Add(1)
-	getLatestRefsDuration.WithLabelValues(label).Observe(time.Since(start).Seconds())
-
-	defer rows.Close()
-
 	ret := make(map[string][]driver.UpdateOperation)
-	for rows.Next() {
-		ops := []driver.UpdateOperation{}
-		ops = append(ops, driver.UpdateOperation{})
-		uo := &ops[len(ops)-1]
-		err := rows.Scan(
-			&uo.Updater,
-			&uo.Ref,
-			&uo.Fingerprint,
-			&uo.Date,
-		)
+	err := s.pool.BeginTxFunc(ctx, pgx.TxOptions{AccessMode: pgx.ReadOnly}, func(tx pgx.Tx) error {
+		defer prometheus.NewTimer(getLatestRefsDuration.WithLabelValues(label)).ObserveDuration()
+		defer getLatestRefsCounter.WithLabelValues(label).Inc()
+		rows, err := tx.Query(ctx, q)
 		if err != nil {
-			rows.Close()
-			return nil, fmt.Errorf("failed to scan update operation for updater %q: %w", uo.Updater, err)
+			return &claircore.Error{
+				Op:      op,
+				Kind:    claircore.ErrInternal,
+				Message: "query error",
+				Inner:   err,
+			}
 		}
-		ret[uo.Updater] = ops
-	}
-	zlog.Debug(ctx).
-		Int("count", len(ret)).
-		Msg("found updaters")
-	return ret, nil
-}
+		defer rows.Close()
 
-func getLatestRefs(ctx context.Context, pool *pgxpool.Pool) (map[string][]driver.UpdateOperation, error) {
-	const query = `SELECT DISTINCT ON (updater) updater, ref, fingerprint, date FROM update_operation ORDER BY updater, id USING >;`
-	ctx = zlog.ContextWithValues(ctx, "component", "internal/vulnstore/postgres/getLatestRefs")
-
-	start := time.Now()
-
-	rows, err := pool.Query(ctx, query)
+		for rows.Next() {
+			ops := []driver.UpdateOperation{}
+			ops = append(ops, driver.UpdateOperation{})
+			uo := &ops[len(ops)-1]
+			err := rows.Scan(
+				&uo.Updater,
+				&uo.Ref,
+				&uo.Fingerprint,
+				&uo.Date,
+			)
+			if err != nil {
+				return &claircore.Error{
+					Op:      op,
+					Kind:    claircore.ErrInternal,
+					Message: fmt.Sprintf("failed to scan update operation for updater %q", uo.Updater),
+					Inner:   err,
+				}
+			}
+			ret[uo.Updater] = ops
+		}
+		if err := rows.Err(); err != nil {
+			return &claircore.Error{
+				Op:      op,
+				Kind:    claircore.ErrInternal,
+				Message: "error deserializing response",
+				Inner:   err,
+			}
+		}
+		return nil
+	})
 	if err != nil {
-		return nil, err
-	}
-
-	getLatestRefsCounter.WithLabelValues("query").Add(1)
-	getLatestRefsDuration.WithLabelValues("query").Observe(time.Since(start).Seconds())
-
-	defer rows.Close()
-
-	ret := make(map[string][]driver.UpdateOperation)
-	for rows.Next() {
-		ops := []driver.UpdateOperation{}
-		ops = append(ops, driver.UpdateOperation{})
-		uo := &ops[len(ops)-1]
-		err := rows.Scan(
-			&uo.Updater,
-			&uo.Ref,
-			&uo.Fingerprint,
-			&uo.Date,
-		)
-		if err != nil {
-			rows.Close()
-			return nil, fmt.Errorf("failed to scan update operation for updater %q: %w", uo.Updater, err)
+		var domErr *claircore.Error
+		if !errors.As(err, &domErr) {
+			domErr = &claircore.Error{
+				Op:      op,
+				Kind:    claircore.ErrInternal,
+				Message: "unexpected database error",
+				Inner:   err,
+			}
 		}
-		ret[uo.Updater] = ops
+		return nil, domErr
 	}
 	zlog.Debug(ctx).
 		Int("count", len(ret)).
@@ -203,53 +198,13 @@ func getLatestRefs(ctx context.Context, pool *pgxpool.Pool) (map[string][]driver
 
 func (s *MatcherStore) GetUpdateOperations(ctx context.Context, kind driver.UpdateKind, updater ...string) (map[string][]driver.UpdateOperation, error) {
 	const (
+		op                 = `datastore/postgres/MatcherStore.GetUpdateOperations`
 		query              = `SELECT ref, updater, fingerprint, date FROM update_operation WHERE updater = ANY($1) ORDER BY id DESC;`
 		queryVulnerability = `SELECT ref, updater, fingerprint, date FROM update_operation WHERE updater = ANY($1) AND kind = 'vulnerability' ORDER BY id DESC;`
 		queryEnrichment    = `SELECT ref, updater, fingerprint, date FROM update_operation WHERE updater = ANY($1) AND kind = 'enrichment' ORDER BY id DESC;`
 		getUpdaters        = `SELECT DISTINCT(updater) FROM update_operation;`
 	)
-	ctx = zlog.ContextWithValues(ctx, "component", "internal/vulnstore/postgres/getUpdateOperations")
-
-	tx, err := s.pool.Begin(ctx)
-	if err != nil {
-		return nil, fmt.Errorf("failed to begin transaction: %w", err)
-	}
-	defer tx.Rollback(ctx)
-	out := make(map[string][]driver.UpdateOperation)
-
-	// Get distinct updaters from database if nothing specified.
-	if len(updater) == 0 {
-		updater = []string{}
-
-		start := time.Now()
-
-		rows, err := tx.Query(ctx, getUpdaters)
-		switch {
-		case err == nil:
-		case errors.Is(err, pgx.ErrNoRows):
-			return out, nil
-		default:
-			return nil, fmt.Errorf("failed to get distinct updates: %w", err)
-		}
-
-		getUpdateOperationsCounter.WithLabelValues("getUpdaters").Add(1)
-		getUpdateOperationsDuration.WithLabelValues("getUpdaters").Observe(time.Since(start).Seconds())
-
-		defer rows.Close() // OK to defer and call, as per docs.
-
-		for rows.Next() {
-			var u string
-			err := rows.Scan(&u)
-			if err != nil {
-				return nil, fmt.Errorf("failed to scan updater: %w", err)
-			}
-			updater = append(updater, u)
-		}
-		if err := rows.Err(); err != nil {
-			return nil, err
-		}
-		rows.Close()
-	}
+	ctx = zlog.ContextWithValues(ctx, "component", op)
 
 	var q string
 	var label string
@@ -265,31 +220,113 @@ func (s *MatcherStore) GetUpdateOperations(ctx context.Context, kind driver.Upda
 		label = "query_vulnerability"
 	}
 
-	start := time.Now()
-	rows, err := tx.Query(ctx, q, updater)
-	switch {
-	case err == nil:
-	case errors.Is(err, pgx.ErrNoRows):
-		return nil, nil
-	default:
-		return nil, fmt.Errorf("failed to get distinct updates: %w", err)
-	}
-	getUpdateOperationsCounter.WithLabelValues(label).Add(1)
-	getUpdateOperationsDuration.WithLabelValues(label).Observe(time.Since(start).Seconds())
-
-	for rows.Next() {
-		var uo driver.UpdateOperation
-		err := rows.Scan(
-			&uo.Ref,
-			&uo.Updater,
-			&uo.Fingerprint,
-			&uo.Date,
-		)
-		if err != nil {
-			rows.Close()
-			return nil, fmt.Errorf("failed to scan update operation for updater %q: %w", uo.Updater, err)
+	// The response (nil, nil) is a valid and expected, so this is initialized
+	// before use inside the closure.
+	var out map[string][]driver.UpdateOperation
+	err := s.pool.BeginTxFunc(ctx, pgx.TxOptions{AccessMode: pgx.ReadOnly}, func(tx pgx.Tx) error {
+		// Get distinct updaters from database if nothing specified.
+		if len(updater) == 0 {
+			if err := func() error {
+				defer prometheus.NewTimer(getUpdateOperationsDuration.WithLabelValues("getUpdaters")).ObserveDuration()
+				defer getUpdateOperationsCounter.WithLabelValues("getUpdaters").Inc()
+				rows, err := tx.Query(ctx, getUpdaters)
+				switch {
+				case err == nil:
+				case errors.Is(err, pgx.ErrNoRows):
+					return nil
+				default:
+					return &claircore.Error{
+						Op:      op,
+						Kind:    claircore.ErrInternal,
+						Message: "failed to get distinct updates",
+						Inner:   err,
+					}
+				}
+				defer rows.Close()
+				for rows.Next() {
+					var u string
+					err := rows.Scan(&u)
+					if err != nil {
+						return &claircore.Error{
+							Op:      op,
+							Kind:    claircore.ErrInternal,
+							Message: "failed to deserialize updater",
+							Inner:   err,
+						}
+					}
+					updater = append(updater, u)
+				}
+				if err := rows.Err(); err != nil {
+					return &claircore.Error{
+						Op:      op,
+						Kind:    claircore.ErrInternal,
+						Message: "error deserializing response",
+						Inner:   err,
+					}
+				}
+				return nil
+			}(); err != nil {
+				return err
+			}
 		}
-		out[uo.Updater] = append(out[uo.Updater], uo)
+
+		defer getUpdateOperationsCounter.WithLabelValues(label).Inc()
+		defer prometheus.NewTimer(getUpdateOperationsDuration.WithLabelValues(label)).ObserveDuration()
+		rows, err := tx.Query(ctx, q, updater)
+		switch {
+		case err == nil:
+		case errors.Is(err, pgx.ErrNoRows):
+			return nil
+		default:
+			return &claircore.Error{
+				Op:      op,
+				Kind:    claircore.ErrInternal,
+				Message: "failed to get distinct updates",
+				Inner:   err,
+			}
+		}
+		defer rows.Close()
+		out = make(map[string][]driver.UpdateOperation)
+
+		for rows.Next() {
+			var uo driver.UpdateOperation
+			err := rows.Scan(
+				&uo.Ref,
+				&uo.Updater,
+				&uo.Fingerprint,
+				&uo.Date,
+			)
+			if err != nil {
+				return &claircore.Error{
+					Op:      op,
+					Kind:    claircore.ErrInternal,
+					Message: "failed to deserialize update operation",
+					Inner:   err,
+				}
+			}
+			out[uo.Updater] = append(out[uo.Updater], uo)
+		}
+		if err := rows.Err(); err != nil {
+			return &claircore.Error{
+				Op:      op,
+				Kind:    claircore.ErrInternal,
+				Message: "error deserializing response",
+				Inner:   err,
+			}
+		}
+		return nil
+	})
+	if err != nil {
+		var domErr *claircore.Error
+		if !errors.As(err, &domErr) {
+			domErr = &claircore.Error{
+				Op:      op,
+				Kind:    claircore.ErrInternal,
+				Message: "unexpected database error",
+				Inner:   err,
+			}
+		}
+		return nil, domErr
 	}
 	return out, nil
 }
