fa5dca1ce1a96057044bb1ca3e4d74553e213b79
Hank Donnay <hdonnay@redhat.com>
2023-02-10 12:35:33 -0600

    postgres: LayerScanned errors
    Signed-off-by: Hank Donnay <hdonnay@redhat.com>

--- datastore/postgres/layerscanned.go
+++ datastore/postgres/layerscanned.go
@@ -4,11 +4,12 @@ import (
 	"context"
 	"errors"
 	"fmt"
-	"time"
 
 	"github.com/jackc/pgx/v4"
+	"github.com/jackc/pgx/v4/pgxpool"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promauto"
+	"github.com/quay/zlog"
 
 	"github.com/quay/claircore"
 	"github.com/quay/claircore/indexer"
@@ -40,6 +41,7 @@ func (s *IndexerStore) LayerScanned(ctx context.Context, hash claircore.Digest, 
 	// TODO(hank) Could this be written as a single query that reports NULL if
 	// the scanner isn't present?
 	const (
+		op            = `datastore/postgres/IndexerStore.LayerScanned`
 		selectScanner = `
 SELECT
 	id
@@ -63,33 +65,72 @@ SELECT
 	);
 `
 	)
+	ctx = zlog.ContextWithValues(ctx, "component", op)
 
-	ctx, done := context.WithTimeout(ctx, 10*time.Second)
-	defer done()
-	start := time.Now()
+	var err error
 	var scannerID int64
-	err := s.pool.QueryRow(ctx, selectScanner, scnr.Name(), scnr.Version(), scnr.Kind()).
-		Scan(&scannerID)
-	switch {
-	case errors.Is(err, nil):
-	case errors.Is(err, pgx.ErrNoRows):
-		return false, fmt.Errorf("scanner %s not found", scnr.Name())
-	default:
-		return false, err
+	err = s.pool.AcquireFunc(ctx, func(c *pgxpool.Conn) error {
+		defer prometheus.NewTimer(layerScannedDuration.WithLabelValues("selectScanner")).ObserveDuration()
+		defer layerScannedCounter.WithLabelValues("selectScanner").Inc()
+		err := s.pool.QueryRow(ctx, selectScanner, scnr.Name(), scnr.Version(), scnr.Kind()).
+			Scan(&scannerID)
+		switch {
+		case errors.Is(err, nil):
+		case errors.Is(err, pgx.ErrNoRows):
+			return &claircore.Error{
+				Op:      op,
+				Kind:    claircore.ErrPrecondition,
+				Message: fmt.Sprintf("scanner %q not found", scnr.Name()),
+				Inner:   err,
+			}
+		default:
+			return &claircore.Error{
+				Op:      op,
+				Kind:    claircore.ErrInternal,
+				Message: "error querying scanner",
+				Inner:   err,
+			}
+		}
+		return nil
+	})
+	if err != nil {
+		var domErr *claircore.Error
+		if !errors.As(err, &domErr) {
+			domErr = &claircore.Error{
+				Op:      op,
+				Kind:    claircore.ErrInternal,
+				Message: "unexpected database error",
+				Inner:   err,
+			}
+		}
+		return false, domErr
 	}
-	layerScannedCounter.WithLabelValues("selectScanner").Add(1)
-	layerScannedDuration.WithLabelValues("selectScanner").Observe(time.Since(start).Seconds())
 
 	var ok bool
-
-	start = time.Now()
-	err = s.pool.QueryRow(ctx, selectScanned, hash.String(), scannerID).
-		Scan(&ok)
+	err = s.pool.AcquireFunc(ctx, func(c *pgxpool.Conn) error {
+		defer prometheus.NewTimer(layerScannedDuration.WithLabelValues("selectScanned")).ObserveDuration()
+		defer layerScannedCounter.WithLabelValues("selectScanned").Inc()
+		if err := c.QueryRow(ctx, selectScanned, hash.String(), scannerID).Scan(&ok); err != nil {
+			return &claircore.Error{
+				Op:      op,
+				Kind:    claircore.ErrInternal,
+				Message: "error querying scanned status",
+				Inner:   err,
+			}
+		}
+		return nil
+	})
 	if err != nil {
-		return false, err
+		var domErr *claircore.Error
+		if !errors.As(err, &domErr) {
+			domErr = &claircore.Error{
+				Op:      op,
+				Kind:    claircore.ErrInternal,
+				Message: "unexpected database error",
+				Inner:   err,
+			}
+		}
+		return false, domErr
 	}
-	layerScannedCounter.WithLabelValues("selectScanned").Add(1)
-	layerScannedDuration.WithLabelValues("selectScanned").Observe(time.Since(start).Seconds())
-
 	return ok, nil
 }
