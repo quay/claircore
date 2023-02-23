8e1dbfee1a5b33390679c7d084f5461d9174ca5a
Arina Stebenkova <arianna.gromova@gmail.com>
2023-02-23 14:34:18 +0200

    suse: change security tracker URL
    The current link lacks relevant CVE
    statistics compared to the suse upstream
    + add new opensuse releases
    
    Signed-off-by: Arina Stebenkova <arianna.gromova@gmail.com>

--- suse/distributionscanner_test.go
+++ suse/distributionscanner_test.go
@@ -8,13 +8,13 @@ import (
 )
 
 var enterpriseServer15OSRelease []byte = []byte(`NAME="SLES"
-VERSION="15-SP1"
+VERSION="15-SP4"
 VERSION_ID="15.1"
-PRETTY_NAME="SUSE Linux Enterprise Server 15 SP1"
+PRETTY_NAME="SUSE Linux Enterprise Server 15 SP4"
 ID="sles"
 ID_LIKE="suse"
 ANSI_COLOR="0;32"
-CPE_NAME="cpe:/o:suse:sles:15:sp1"`)
+CPE_NAME="cpe:/o:suse:sles:15:sp4"`)
 
 var enterpriseServer12OSRelase []byte = []byte(`NAME="SLES"
 VERSION="12-SP5"
@@ -32,6 +32,39 @@ ID="sles"
 ANSI_COLOR="0;32"
 CPE_NAME="cpe:/o:suse:sles:12:sp5"`)
 
+var leap154OSRelease []byte = []byte(`NAME="openSUSE Leap"
+VERSION="15.4"
+ID="opensuse-leap"
+ID_LIKE="suse opensuse"
+VERSION_ID="15.4"
+PRETTY_NAME="openSUSE Leap 15.4"
+ANSI_COLOR="0;32"
+CPE_NAME="cpe:/o:opensuse:leap:15.4"
+BUG_REPORT_URL="https://bugzilla.opensuse.org"
+HOME_URL="https://www.opensuse.org/"`)
+
+var leap153OSRelease []byte = []byte(`NAME="openSUSE Leap"
+VERSION="15.3"
+ID="opensuse-leap"
+ID_LIKE="suse opensuse"
+VERSION_ID="15.3"
+PRETTY_NAME="openSUSE Leap 15.3"
+ANSI_COLOR="0;32"
+CPE_NAME="cpe:/o:opensuse:leap:15.3"
+BUG_REPORT_URL="https://bugzilla.opensuse.org"
+HOME_URL="https://www.opensuse.org/"`)
+
+var leap152OSRelease []byte = []byte(`NAME="openSUSE Leap"
+VERSION="15.2"
+ID="opensuse-leap"
+ID_LIKE="suse opensuse"
+VERSION_ID="15.2"
+PRETTY_NAME="openSUSE Leap 15.2"
+ANSI_COLOR="0;32"
+CPE_NAME="cpe:/o:opensuse:leap:15.2"
+BUG_REPORT_URL="https://bugzilla.opensuse.org"
+HOME_URL="https://www.opensuse.org/"`)
+
 var leap151OSRelease []byte = []byte(`NAME="openSUSE Leap"
 VERSION="15.1"
 ID="opensuse-leap"
@@ -40,7 +73,7 @@ VERSION_ID="15.1"
 PRETTY_NAME="openSUSE Leap 15.1"
 ANSI_COLOR="0;32"
 CPE_NAME="cpe:/o:opensuse:leap:15.1"
-BUG_REPORT_URL="https://bugs.opensuse.org"
+BUG_REPORT_URL="https://bugzilla.opensuse.org"
 HOME_URL="https://www.opensuse.org/"`)
 
 var leap15OSRelease []byte = []byte(`NAME="openSUSE Leap"
@@ -51,7 +84,7 @@ VERSION_ID="15.0"
 PRETTY_NAME="openSUSE Leap 15.0"
 ANSI_COLOR="0;32"
 CPE_NAME="cpe:/o:opensuse:leap:15.0"
-BUG_REPORT_URL="https://bugs.opensuse.org"
+BUG_REPORT_URL="https://bugzilla.opensuse.org"
 HOME_URL="https://www.opensuse.org/"`)
 
 var leap423OSRelease []byte = []byte(`NAME="openSUSE Leap"
@@ -62,7 +95,7 @@ VERSION_ID="42.3"
 PRETTY_NAME="openSUSE Leap 42.3"
 ANSI_COLOR="0;32"
 CPE_NAME="cpe:/o:opensuse:leap:42.3"
-BUG_REPORT_URL="https://bugs.opensuse.org"
+BUG_REPORT_URL="https://bugzilla.opensuse.org"
 HOME_URL="https://www.opensuse.org/"`)
 
 func TestDistributionScanner(t *testing.T) {
@@ -87,14 +120,29 @@ func TestDistributionScanner(t *testing.T) {
 			osRelease: enterpriseServer11OSRelease,
 		},
 		{
-			name:      "leap 15.0",
-			release:   Leap150,
-			osRelease: leap15OSRelease,
+			name:      "leap 15.4",
+			release:   Leap154,
+			osRelease: leap154OSRelease,
+		},
+		{
+			name:      "leap 15.3",
+			release:   Leap153,
+			osRelease: leap153OSRelease,
+		},
+		{
+			name:      "leap 15.2",
+			release:   Leap152,
+			osRelease: leap152OSRelease,
 		},
 		{
 			name:      "leap 15.1",
 			release:   Leap151,
 			osRelease: leap151OSRelease,
+		},
+		{
+			name:      "leap 15.0",
+			release:   Leap150,
+			osRelease: leap15OSRelease,
 		},
 		{
 			name:      "leap 42.3",
