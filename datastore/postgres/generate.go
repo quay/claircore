9ac012d165baca84da05815c6d6728ef602efb36
Hank Donnay <hdonnay@redhat.com>
2023-03-01 14:50:14 -0600

    postgres: fix generate directive
    The relative path changed with the database consolidation. This should
    always work correctly.
    
    Signed-off-by: Hank Donnay <hdonnay@redhat.com>

--- datastore/postgres/generate.go
+++ datastore/postgres/generate.go
@@ -4,5 +4,5 @@ package postgres
 //
 // Splitting it is not allowed.
 
-//go:generate -command mktestdata go run ../../../test/bisect -dump-index "testdata/{{.}}.index.json" -dump-report "testdata/{{.}}.report.json"
+//go:generate -command mktestdata go run github.com/quay/claircore/test/bisect -dump-index "testdata/{{.}}.index.json" -dump-report "testdata/{{.}}.report.json"
 //go:generate mktestdata docker.io/library/amazonlinux:1 docker.io/library/debian:10 docker.io/library/debian:9 docker.io/library/debian:8 docker.io/mitmproxy/mitmproxy:4.0.1 docker.io/library/ubuntu:16.04 docker.io/library/ubuntu:18.04 docker.io/library/ubuntu:19.10 docker.io/library/ubuntu:20.04 registry.access.redhat.com/ubi8/ubi
