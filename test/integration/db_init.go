b0e0723b691c204b4d9d5ceb83cc8fe320cb4cf6
Hank Donnay <hdonnay@redhat.com>
2023-03-02 21:49:59 -0600

    integration: update database versions, defaults
    I wanted to get WAL buffer information out EXPLAIN, which was added
    since postgres 12. This updates the versions and sets the default to the
    latest.
    
    Signed-off-by: Hank Donnay <hdonnay@redhat.com>

--- test/integration/db_init.go
+++ test/integration/db_init.go
@@ -3,6 +3,7 @@ package integration
 import (
 	"os"
 	"runtime"
+	"strings"
 )
 
 var (
@@ -13,21 +14,28 @@ var (
 
 func init() {
 	dbOS = runtime.GOOS
-	dbVersion = v12
+	dbVersion = v15
 	dbArch = runtime.GOARCH
 
 	// See if a different version was requested
-	switch e := os.Getenv(EnvPGVersion); e {
-	case "13":
-		dbVersion = v13
-	case "12":
-		// default, set above
-	case "11":
-		dbVersion = v11
-	case "10":
-		dbVersion = v10
-	case "9":
-		dbVersion = v9
+	if e := os.Getenv(EnvPGVersion); e != "" {
+		if strings.Count(e, ".") == 2 {
+			// Try it as a version string 🤷
+			dbVersion = e
+		}
+		switch e {
+		case "15":
+			dbVersion = v15
+		case "14":
+			dbVersion = v14
+		case "13":
+			dbVersion = v13
+		case "12":
+			dbVersion = v12
+		case "11":
+			dbVersion = v11
+		default: // Ignore, use what it's been set to.
+		}
 	}
 
 	// This is a per-OS function.
--- test/integration/db_versions.go
+++ test/integration/db_versions.go
@@ -1,9 +1,11 @@
 package integration
 
+// See https://repo1.maven.org/maven2/io/zonky/test/postgres/embedded-postgres-binaries-linux-amd64/maven-metadata.xml
+// for the list of versions.
 const (
-	v13 = "13.2.0"
-	v12 = "12.6.0"
-	v11 = "11.11.0"
-	v10 = "10.16.0"
-	v9  = "9.6.21"
+	v15 = "15.2.0"
+	v14 = "14.7.0"
+	v13 = "13.10.0"
+	v12 = "12.14.0"
+	v11 = "11.19.0"
 )
