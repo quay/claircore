d6cb605f45b8b3123549c63d69f21bfacf78cc9c
Hank Donnay <hdonnay@redhat.com>
2023-03-13 15:40:29 -0500

    rpm: consult dnf database for repository information
    This change consults the dnf history database for the repository a package came
    from, unless the layer has some features that we know means the results will be
    useless.
    
    See-also: #809
    Signed-off-by: Hank Donnay <hdonnay@redhat.com>

---
+++ rpm/dnf_finalstate.sql
@@ -0,0 +1,27 @@
+SELECT
+	name ||'-'||
+	CASE
+		WHEN epoch = 0 THEN ''
+		ELSE epoch || ':'
+	END ||
+	version ||'-'||
+	release ||'.'||
+	arch
+		AS nerva,
+	repoid
+FROM
+	trans_item
+	JOIN (
+		SELECT
+			max(id) AS uniq
+		FROM
+			trans_item
+		WHERE
+			action <> ?
+		GROUP BY
+			item_id
+		) ON (uniq = trans_item.id)
+	JOIN
+		repo ON (repo.id = repo_id)
+	JOIN
+		rpm USING (item_id);
