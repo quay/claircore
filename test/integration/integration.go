55bb23f4117c9a6fdc39c82f4c4cdba5d8f00e31
Hank Donnay <hdonnay@redhat.com>
2023-05-17 16:59:46 -0500

    integration: add native support for Github Actions
    This change has the `integration` package take notice of the environment
    variables that indicate that it's running in CI in Github Actions and
    subsequently run the integration tests despite the absence of a build
    tag and issue commands to control the local postgres instance directly.
    
    Signed-off-by: Hank Donnay <hdonnay@redhat.com>

--- test/integration/integration.go
+++ test/integration/integration.go
@@ -2,18 +2,33 @@
 package integration
 
 import (
+	"os"
+	"strconv"
 	"testing"
 )
 
 // Skip will skip the current test or benchmark if this package was built without
-// the "integration" build tag.
+// the "integration" build tag unless the "CI" environment variable is defined
+// and the "short" flag is not provided.
 //
 // This should be used as an annotation at the top of the function, like
 // (*testing.T).Parallel().
 //
 // See the example for usage.
 func Skip(t testing.TB) {
-	if skip {
+	switch {
+	case testing.Short():
+		t.Skip(`skipping integration test: short tests`)
+	case inCI && skip:
+		t.Log(`enabling integration test: environment variable "CI" is defined`)
+	case skip:
 		t.Skip("skipping integration test: integration tag not provided")
 	}
+}
+
+var inCI, inGHA bool
+
+func init() {
+	inCI, _ = strconv.ParseBool(os.Getenv("CI"))
+	inGHA, _ = strconv.ParseBool(os.Getenv("GITHUB_ACTIONS"))
 }
