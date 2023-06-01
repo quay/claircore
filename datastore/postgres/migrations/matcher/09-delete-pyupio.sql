8bc2dc3346ca0a8c184a71931de20320124bc8f5
crozzy <joseph.crosland@gmail.com>
2023-06-01 09:30:52 -0700

    python: Delete pyupio vulns
    Because no more update_operations are being created for pyupio the
    current vulns will not be GCed. This is a manual intervention to
    clear them up and avoid double reporting.
    
    Signed-off-by: crozzy <joseph.crosland@gmail.com>

---
+++ datastore/postgres/migrations/matcher/09-delete-pyupio.sql
@@ -0,0 +1,10 @@
+-- Delete all update_operations for pyupio to trigger pyupio vuln deletion
+DELETE FROM update_operation WHERE updater = 'pyupio';
+
+DELETE FROM vuln v1 USING
+	vuln v2
+	LEFT JOIN uo_vuln uvl
+		ON v2.id = uvl.vuln
+	WHERE uvl.vuln IS NULL
+	AND v2.updater = 'pyupio'
+AND v1.id = v2.id;
