14dca712b692b6f60c70dd0a8ea61762304aac39
Hank Donnay <hdonnay@redhat.com>
2023-09-12 15:24:05 -0500

    whiteout: update tests
    Signed-off-by: Hank Donnay <hdonnay@redhat.com>

--- whiteout/scanner_test.go
+++ whiteout/scanner_test.go
@@ -2,109 +2,78 @@ package whiteout
 
 import (
 	"context"
+	"os"
 	"testing"
 
 	"github.com/quay/zlog"
 
 	"github.com/quay/claircore"
+	"github.com/quay/claircore/test"
 )
 
-// files in whiteout.layer
-// etc/
-// etc/.wh.profile
-func TestBasicWhiteout(t *testing.T) {
-	t.Parallel()
-	const layerfile = `testdata/whiteout.layer`
-	l := claircore.Layer{
-		Hash: claircore.MustParseDigest(`sha256:25fd87072f39aaebd1ee24dca825e61d9f5a0f87966c01551d31a4d8d79d37d8`),
-		URI:  "file:///dev/null",
-	}
+var testScanner = &Scanner{}
+
+// CheckCount is a helper to check the number of whiteouts returned from the
+// layer in "name".
+func checkCount(t testing.TB, name string, ct int) {
+	t.Helper()
 	ctx := zlog.Test(context.Background(), t)
-
-	// Set up the crafted layer
-	l.SetLocal(layerfile)
-	if t.Failed() {
-		return
+	lf, err := os.Open(name)
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Cleanup(func() {
+		if err := lf.Close(); err != nil {
+			t.Error(err)
+		}
+	})
+	var l claircore.Layer
+	err = l.Init(ctx, &claircore.LayerDescription{
+		Digest:    `sha256:25fd87072f39aaebd1ee24dca825e61d9f5a0f87966c01551d31a4d8d79d37d8`,
+		URI:       "file:///dev/null",
+		MediaType: test.MediaType,
+		Headers:   make(map[string][]string),
+	}, lf)
+	if err != nil {
+		t.Fatal(err)
 	}
 
-	s := new(Scanner)
-	files, err := s.Scan(ctx, &l)
+	files, err := testScanner.Scan(ctx, &l)
 	if err != nil {
 		t.Error(err)
 	}
-	if got, want := len(files), 1; got != want {
+	if got, want := len(files), ct; got != want {
 		t.Errorf("checking length, got: %d, want: %d", got, want)
 	}
-
 	for _, f := range files {
-		t.Logf("got whiteout file %s", f.Path)
+		t.Logf("got whiteout file %q", f.Path)
 	}
 }
 
-// files in whiteout_advanced.layer
-// a/
-// a/.wh.a_file.txt
-// .wh.a_root_file.txt
-// .wh.b
-func TestAdvancedWhiteout(t *testing.T) {
-	t.Parallel()
-	const layerfile = `testdata/whiteout_advanced.layer`
-	l := claircore.Layer{
-		Hash: claircore.MustParseDigest(`sha256:25fd87072f39aaebd1ee24dca825e61d9f5a0f87966c01551d31a4d8d79d37d8`),
-		URI:  "file:///dev/null",
-	}
-	ctx := zlog.Test(context.Background(), t)
-
-	// Set up the crafted layer
-	l.SetLocal(layerfile)
-	if t.Failed() {
-		return
-	}
-
-	s := new(Scanner)
-	files, err := s.Scan(ctx, &l)
-	if err != nil {
-		t.Error(err)
-	}
-	if got, want := len(files), 3; got != want {
-		t.Errorf("checking length, got: %d, want: %d", got, want)
-	}
-
-	for _, f := range files {
-		t.Logf("got whiteout file %s", f.Path)
-	}
-}
-
-// files in whiteout_opaque.layer
-// a/
-// a/.wh..wh..opq
-// b/
-// b/not.a.wh..wh..opq
-func TestOpaqueWhiteout(t *testing.T) {
-	t.Parallel()
-	const layerfile = `testdata/whiteout_opaque.layer`
-	l := claircore.Layer{
-		Hash: claircore.MustParseDigest(`sha256:25fd87072f39aaebd1ee24dca825e61d9f5a0f87966c01551d31a4d8d79d37d8`),
-		URI:  "file:///dev/null",
-	}
-	ctx := zlog.Test(context.Background(), t)
-
-	// Set up the crafted layer
-	l.SetLocal(layerfile)
-	if t.Failed() {
-		return
-	}
-
-	s := new(Scanner)
-	files, err := s.Scan(ctx, &l)
-	if err != nil {
-		t.Error(err)
-	}
-	if got, want := len(files), 1; got != want {
-		t.Errorf("checking length, got: %d, want: %d", got, want)
-	}
-
-	for _, f := range files {
-		t.Logf("got whiteout file %s", f.Path)
-	}
+func TestWhiteout(t *testing.T) {
+	t.Run("Basic", func(t *testing.T) {
+		t.Parallel()
+		// files in whiteout.layer
+		// etc/
+		// etc/.wh.profile
+		checkCount(t, `testdata/whiteout.layer`, 1)
+	})
+	t.Run("Advanced", func(t *testing.T) {
+		t.Parallel()
+		// files in whiteout_advanced.layer
+		// a/
+		// a/.wh.a_file.txt
+		// .wh.a_root_file.txt
+		// .wh.b
+		checkCount(t, `testdata/whiteout_advanced.layer`, 3)
+	})
+	t.Run("Opaque", func(t *testing.T) {
+		t.Parallel()
+		// files in whiteout_opaque.layer
+		// a/
+		// a/.wh..wh..opq
+		// b/
+		// b/not.a.wh..wh..opq
+		checkCount(t, `testdata/whiteout_opaque.layer`, 1)
+	})
 }
