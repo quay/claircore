6f0ebff4186865f893d1631d4efcd8097626700e
Hank Donnay <hdonnay@redhat.com>
2023-09-27 18:11:47 -0500

    postgres: unify test database creation functions
    Signed-off-by: Hank Donnay <hdonnay@redhat.com>

---
+++ test/postgres/doc.go
@@ -0,0 +1,18 @@
+// Package postgres contains testing helpers for PostgreSQL databases.
+//
+// Most of the functions provide independent implementations of functionality
+// that the datastore/postgres package does internally in its exposed API.
+//
+// # Test Databases
+//
+// The [TestIndexerDB] and [TestMatcherDB] start database engines and create
+// suitable databases as needed. See the [test/integration] package for more
+// information on the specifics of the former.
+//
+// As part of the database setup, these functions can load SQL files. This
+// package searches both its embedded copy of the local "sql" directory and the
+// main test package's "testdata" director for these patterns:
+//   - all_*.psql
+//   - matcher_*.psql (for [TestMatcherDB])
+//   - indexer_*.psql (for [TestIndexerDB])
+package postgres
