2c13e5a59454a7a450e8b6f27102e4bc7ac30671
Hank Donnay <hdonnay@redhat.com>
2023-10-10 17:16:37 -0500

    claircore: documentation typos
    Signed-off-by: Hank Donnay <hdonnay@redhat.com>

--- file.go
+++ file.go
@@ -1,13 +1,13 @@
 package claircore
 
-// FileKind is used to detemine what kind of file was found.
+// FileKind is used to determine what kind of file was found.
 type FileKind string
 
 const (
 	FileKindWhiteout = FileKind("whiteout")
 )
 
-// File represents interesing files that are found in the layer.
+// File represents interesting files that are found in the layer.
 type File struct {
 	// Path is where in the layer filesystem the file is located.
 	Path string
