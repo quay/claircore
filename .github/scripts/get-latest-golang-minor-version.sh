417c2b0d2cbfe3cc7bf9db55990a2a38c779e689
Hank Donnay <hdonnay@redhat.com>
2024-03-18 17:18:46 -0500

    cicd: update minor-version script
    This uses the JSON response from the go website instead of guessing at
    URLs.
    
    Signed-off-by: Hank Donnay <hdonnay@redhat.com>

--- .github/scripts/get-latest-golang-minor-version.sh
+++ .github/scripts/get-latest-golang-minor-version.sh
@@ -1,26 +1,17 @@
 #!/bin/sh
 MAJOR_VERSION=${1?missing required argument: language version}
-CHECK_VERSION=$MAJOR_VERSION
-CHECK_MINOR_VERSION=0
 
-while true; do
-    URL=https://dl.google.com/go/go${CHECK_VERSION}.linux-amd64.tar.gz
-	code=$(curl -I -o /dev/null -s -w "%{http_code}\n" "$URL")
-	printf '%s\t%d\n' "$URL" "$code" >&2
-	case "$code" in
-	200)
-		OK="$CHECK_VERSION"
-		CHECK_VERSION=${MAJOR_VERSION}.$((CHECK_MINOR_VERSION+=1))
-		;;
-	*)
-		# Account for version scheme change. See issue #991.
-		if test -z "$(echo "${CHECK_VERSION}" | cut -d . -f 3 2>/dev/null)"; then
-			CHECK_VERSION=${MAJOR_VERSION}.${CHECK_MINOR_VERSION}
-			continue
-		fi
-		test -z "$OK" && exit 99
-		echo "$OK"
-		exit 0
-		;;
-	esac
-done
+curl -sSfL 'https://golang.org/dl/?mode=json&include=all' |
+jq -r --arg 'v' "$MAJOR_VERSION" '[
+	.[] |
+	.version |
+	select(
+		[
+			contains("go\($v)"),
+			(contains("rc") | not)
+		] |
+		all
+	) |
+	ltrimstr("go")
+] |
+first'
