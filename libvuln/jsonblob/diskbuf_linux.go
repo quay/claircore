a08445da2a77ab0d94688516a278097a8e2d48ca
Hank Donnay <hdonnay@redhat.com>
2024-04-11 16:39:35 -0500

    jsonblob: undo overzealous use of decade-old kernel feature
    Closes: #1313
    Signed-off-by: Hank Donnay <hdonnay@redhat.com>

--- libvuln/jsonblob/diskbuf_linux.go
+++ libvuln/jsonblob/diskbuf_linux.go
@@ -2,11 +2,25 @@ package jsonblob
 
 import (
 	"context"
+	"errors"
 	"os"
-
-	"golang.org/x/sys/unix"
 )
 
+// BUG(hank) On Linux, the disk buffering unconditionally uses what [os.TempDir]
+// reports. On most systems, this will be a tmpfs, which means the contents are
+// stored in RAM anyway. To mitigate this, set the "TMPDIR" environment variable
+// for any process that's using this package.
+
+// TODO(hank) Consolidate the spool/tempfile logic, or at least the directory
+// selection logic.
+
 func diskBuf(_ context.Context) (*os.File, error) {
-	return os.OpenFile(os.TempDir(), os.O_RDWR|unix.O_TMPFILE, 0600)
+	f, err := os.CreateTemp("", "jsonblob.")
+	if err != nil {
+		return nil, err
+	}
+	if err := os.Remove(f.Name()); err != nil {
+		return nil, errors.Join(err, f.Close())
+	}
+	return f, nil
 }
