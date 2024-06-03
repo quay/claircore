1ccd38ba6fe3f5b4d7a2e5806ec6eb4ac8f32170
RTann <rtannenb@redhat.com>
2024-06-03 11:09:57 -0700

    chainguard: add chainguard and wolfi support
    Signed-off-by: RTann <rtannenb@redhat.com>

---
+++ chainguard/ecosystem.go
@@ -0,0 +1,27 @@
+package chainguard
+
+import (
+	"context"
+
+	"github.com/quay/claircore/apk"
+	"github.com/quay/claircore/indexer"
+	"github.com/quay/claircore/linux"
+)
+
+// NewEcosystem provides the set of scanners and coalescers for the chainguard ecosystem
+func NewEcosystem(_ context.Context) *indexer.Ecosystem {
+	return &indexer.Ecosystem{
+		PackageScanners: func(ctx context.Context) ([]indexer.PackageScanner, error) {
+			return []indexer.PackageScanner{&apk.Scanner{}}, nil
+		},
+		DistributionScanners: func(ctx context.Context) ([]indexer.DistributionScanner, error) {
+			return []indexer.DistributionScanner{&DistributionScanner{}}, nil
+		},
+		RepositoryScanners: func(ctx context.Context) ([]indexer.RepositoryScanner, error) {
+			return []indexer.RepositoryScanner{}, nil
+		},
+		Coalescer: func(ctx context.Context) (indexer.Coalescer, error) {
+			return linux.NewCoalescer(), nil
+		},
+	}
+}
