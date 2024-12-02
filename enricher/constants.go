aa256e64666813c567a9315f94795eb796705616
daynewlee <yli3@redhat.com>
2024-12-02 09:24:04 -0600

    epss: added shared constants
    Signed-off-by: daynewlee <yli3@redhat.com>

---
+++ enricher/constants.go
@@ -0,0 +1,10 @@
+package enricher
+
+import "regexp"
+
+// This is a slightly more relaxed version of the validation pattern in the NVD
+// JSON schema: https://csrc.nist.gov/schema/nvd/feed/1.1/CVE_JSON_4.0_min_1.1.schema
+//
+// It allows for "CVE" to be case insensitive and for dashes and underscores
+// between the different segments.
+var CVERegexp = regexp.MustCompile(`(?i:cve)[-_][0-9]{4}[-_][0-9]{4,}`)
