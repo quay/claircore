5e7877e84ea36b4de261c0f62e53882f6d14efe4
Hank Donnay <hdonnay@redhat.com>
2025-01-06 15:00:31 -0600

    internal/rpm: new iterator-based rpm package
    Signed-off-by: Hank Donnay <hdonnay@redhat.com>

---
+++ internal/rpm/ndb/doc.go
@@ -0,0 +1,4 @@
+// Package ndb provides support for read-only access to an RPM "ndb" database.
+//
+// The support for ndb's native indexes is probably unneeded, but is implemented for completeness.
+package ndb
