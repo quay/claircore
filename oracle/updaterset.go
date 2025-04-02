1e7ab55d595215eb5580fe3e8a642659b5645529
Brad Lugo <blugo@redhat.com>
2025-04-02 10:03:55 -0700

    oracle: inclusive year limit in updater
    Signed-off-by: Brad Lugo <blugo@redhat.com>

--- oracle/updaterset.go
+++ oracle/updaterset.go
@@ -10,7 +10,7 @@ import (
 
 func UpdaterSet(_ context.Context) (driver.UpdaterSet, error) {
 	us := driver.NewUpdaterSet()
-	for year, lim := 2007, time.Now().Year(); year != lim; year++ {
+	for year, lim := 2007, time.Now().Year(); year <= lim; year++ {
 		u, err := NewUpdater(year)
 		if err != nil {
 			return us, fmt.Errorf("unable to create oracle updater: %v", err)
