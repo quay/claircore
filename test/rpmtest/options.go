df86bf13701d143257d78891b78ff36206d46cbf
Hank Donnay <hdonnay@redhat.com>
2025-04-23 12:23:23 -0500

    rpmtest: change Options for better test output
    Signed-off-by: Hank Donnay <hdonnay@redhat.com>

--- test/rpmtest/options.go
+++ test/rpmtest/options.go
@@ -3,6 +3,8 @@ package rpmtest
 import (
 	"net/url"
 	"reflect"
+	"sync"
+	"testing"
 
 	"github.com/google/go-cmp/cmp"
 	"github.com/google/go-cmp/cmp/cmpopts"
@@ -12,52 +14,68 @@ import (
 )
 
 // Options is a standard set of [cmp.Options] for working with packages from
-// rpm.
-var Options = cmp.Options{
-	HintCompare,
-	VersionCompare,
-	IgnorePackageDB,
-	SortPackages,
-	ModuleCompare,
+// rpm. The passed [testing.TB] is captured, so the returned Options cannot be
+// reused across tests.
+func Options(t testing.TB) cmp.Options {
+	return cmp.Options{
+		HintCompare(t),
+		VersionTransform(t),
+		IgnorePackageDB,
+		SortPackages,
+		ModuleCompare,
+	}
 }
 
 // HintCompare normalizes the claircore-internal "hint".
 //
 // The RPM manifest doesn't have checksum information. It does have keyid
 // information, so normalize down to the common set.
-var HintCompare = cmp.FilterPath(
-	func(p cmp.Path) bool { return p.Last().String() == ".RepositoryHint" },
-	cmp.Comparer(func(a, b string) bool {
-		av, err := url.ParseQuery(a)
-		if err != nil {
-			panic(err)
-		}
-		bv, err := url.ParseQuery(b)
-		if err != nil {
-			panic(err)
-		}
-		av.Del("hash")
-		bv.Del("hash")
-		return cmp.Equal(av.Encode(), bv.Encode())
-	}),
-)
+func HintCompare(t testing.TB) cmp.Option {
+	return cmp.FilterPath(
+		func(p cmp.Path) bool { return p.Last().String() == ".RepositoryHint" },
+		cmp.Comparer(func(a, b string) bool {
+			av, err := url.ParseQuery(a)
+			if err != nil {
+				t.Errorf("%q: %v", a, err)
+			}
+			bv, err := url.ParseQuery(b)
+			if err != nil {
+				t.Errorf("%q: %v", b, err)
+			}
+			av.Del("hash")
+			bv.Del("hash")
+			return cmp.Equal(av.Encode(), bv.Encode())
+		}),
+	)
+}
 
-// VersionCompare turns a [Package.Version] into [rpmver.Version]. Go-cmp
+// VersionTransform turns a [Package.Version] into [rpmver.Version]. Go-cmp
 // produces sensible output on [rpmver.Version] objects.
-var VersionCompare = cmp.FilterPath(
-	func(p cmp.Path) bool {
-		l := p.Last()
-		return l.Type() == reflect.TypeFor[claircore.Package]() && l.String() == ".Version"
-	},
-	cmp.Transformer("ParseRPMVersion", func(v string) rpmver.Version {
-		println(v)
-		p, err := rpmver.Parse(v)
-		if err != nil {
-			panic(v + ": " + err.Error())
-		}
-		return p
-	}),
-)
+func VersionTransform(t testing.TB) cmp.Option {
+	var warn sync.Once
+	return cmp.FilterPath(
+		func(p cmp.Path) bool {
+			prev := p.Index(-2)
+			cur := p.Last()
+			return prev.Type() == reflect.TypeFor[claircore.Package]() &&
+				(cur.String() == ".Version" && cur.Type() == reflect.TypeFor[string]())
+		},
+		cmp.Transformer("ParseRPMVersion", func(v string) rpmver.Version {
+			p, err := rpmver.Parse(v)
+			if err != nil {
+				t.Errorf("%q: %v", v, err)
+			}
+			if p.Epoch != "" {
+				warn.Do(func() {
+					t.Log("⚠️\tunable to compare versions with epochs (see https://issues.redhat.com/browse/KONFLUX-7481)")
+					t.Log("⚠️\tsetting all epochs to \"0\"")
+				})
+			}
+			p.Epoch = ""
+			return p
+		}),
+	)
+}
 
 // ModuleCompare allows one of the reported modules to be the empty string.
 // This is needed because of [KONFLUX-7481] (née [STONEBLD-1472]).
