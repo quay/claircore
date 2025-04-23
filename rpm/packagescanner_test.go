758a874480f5f62c8528bd82454d5c6a2ca58150
Hank Donnay <hdonnay@redhat.com>
2025-04-23 12:11:55 -0500

    rpm: move tests to manifest archives
    Signed-off-by: Hank Donnay <hdonnay@redhat.com>

--- rpm/packagescanner_test.go
+++ rpm/packagescanner_test.go
@@ -12,12 +12,24 @@ import (
 	"github.com/quay/zlog"
 
 	"github.com/quay/claircore"
+	"github.com/quay/claircore/internal/wart"
 	"github.com/quay/claircore/test"
 	"github.com/quay/claircore/test/rpmtest"
 )
 
-func TestScan(t *testing.T) {
+//go:generate -command fetch go run github.com/quay/claircore/test/cmd/fetch-container-rpm-manifest
+//go:generate fetch -o testdata/package/ubi8_ubi.txtar ubi8/ubi
+//go:generate fetch -o testdata/package/ubi9_ubi.txtar ubi9/ubi
+// The nodejs images are currently broken:
+////go:generate fetch -o testdata/package/ubi8_nodejs.txtar ubi8/nodejs-*
+////go:generate fetch -o testdata/package/ubi9_nodejs.txtar ubi9/nodejs-20
+
+func TestPackageDetection(t *testing.T) {
 	ctx := zlog.Test(context.Background(), t)
+	ms, err := filepath.Glob("testdata/package/*.txtar")
+	if err != nil {
+		panic("programmer error") // static glob
+	}
 
 	a := test.NewCachedArena(t)
 	t.Cleanup(func() {
@@ -26,104 +38,82 @@ func TestScan(t *testing.T) {
 		}
 	})
 
-	tt := []PackageTestcase{
-		{
-			Name:         "BDB",
-			ManifestFile: "bdb.rpm-manifest.json",
-			// Some forgotten CentOS layer.
-			Ref: test.LayerRef{
-				Registry: "docker.io",
-				Name:     "library/centos",
-				Digest:   `sha256:729ec3a6ada3a6d26faca9b4779a037231f1762f759ef34c08bdd61bf52cd704`,
-			},
-		},
-		{
-			Name:         "SQLite",
-			ManifestFile: "sqlite.rpm-manifest.json",
-			// Layer from registry.access.redhat.com/ubi9/ubi@sha256:f14f9eb6c5ec6ea9664b54ea8816462e11abc096954d59eac536b64873d908f2
-			Ref: test.LayerRef{
-				Registry: "registry.access.redhat.com",
-				Name:     "ubi9/ubi",
-				Digest:   `sha256:6505b024e539929a1909c8820535221fe70806ce5467b5f30aef8d45a4a97da7`,
-			},
-		},
-		{
-			Name:         "NodeJS",
-			ManifestFile: "nodejs.rpm-manifest.json",
-			// Layer from registry.access.redhat.com/ubi9/nodejs-18@sha256:1ff5080686736cbab820ec560873c59bd80659a2b2f8d8f4e379301a910e5d54
-			Ref: test.LayerRef{
-				Registry: "registry.access.redhat.com",
-				Name:     "ubi9/nodejs-18",
-				Digest:   `sha256:1ae06b64755052cef4c32979aded82a18f664c66fa7b50a6d2924afac2849c6e`,
-			},
-		},
-	}
-	for _, tc := range tt {
-		t.Run(tc.Name, tc.Run(ctx, a))
-	}
-}
-
-type PackageTestcase struct {
-	Name         string
-	ManifestFile string
-	Ref          test.LayerRef
-}
-
-func (tc PackageTestcase) Run(ctx context.Context, a *test.CachedArena) func(*testing.T) {
-	s := &Scanner{}
-	return func(t *testing.T) {
-		t.Parallel()
-		ctx := zlog.Test(ctx, t)
-		a.LoadLayerFromRegistry(ctx, t, tc.Ref)
-		wf, err := os.Open(filepath.Join("testdata/", tc.ManifestFile))
-		if err != nil {
-			t.Fatal(err)
-		}
-		t.Cleanup(func() {
-			if err := wf.Close(); err != nil {
-				t.Error(err)
-			}
-		})
-		want := rpmtest.PackagesFromRPMManifest(t, wf)
-		r := a.Realizer(ctx).(*test.CachedRealizer)
-		t.Cleanup(func() {
-			if err := r.Close(); err != nil {
-				t.Error(err)
-			}
-		})
-		ls, err := r.RealizeDescriptions(ctx, []claircore.LayerDescription{
-			{
-				Digest:    tc.Ref.Digest,
-				URI:       "http://example.com",
-				MediaType: test.MediaType,
-				Headers:   make(map[string][]string),
-			},
-		})
-		if err != nil {
-			t.Fatal(err)
-		}
-
-		got, err := s.Scan(ctx, &ls[0])
+	for _, m := range ms {
+		ar, err := rpmtest.OpenArchive(ctx, m)
 		if err != nil {
 			t.Error(err)
+			continue
 		}
-		slices.SortFunc(got, func(a, b *claircore.Package) int {
-			return strings.Compare(a.Name, b.Name)
-		})
-		t.Logf("found %d packages, want %d", len(got), len(want))
-		if gotLen, wantLen := len(got), len(want); gotLen != wantLen {
-			t.Fail()
-		}
-		for i := range want {
-			got, want := got[i], want[i]
-			diff := cmp.Diff(got, want, rpmtest.Options)
-			if diff == "" {
-				diff = "OK"
-			}
-			t.Logf("%s:\t%v\n", want.Name, diff)
-			if !cmp.Equal(got, want, rpmtest.Options) {
-				t.Fail()
-			}
+
+		for _, ref := range ar.Repositories() {
+			t.Run(ref, func(t *testing.T) {
+				t.Parallel()
+
+				arches := map[string]struct{}{}
+				reg, repo, _ := strings.Cut(ref, "/")
+				imgs, err := ar.Image(reg, repo)
+				if err != nil {
+					t.Fatal(err)
+				}
+
+				for _, img := range imgs.Data {
+					// Only consider the first listed instance of an
+					// architecture, since that's the only rpm manifest that
+					// will be populated.
+					if _, skip := arches[img.Archtecture]; skip {
+						continue
+					}
+					arches[img.Archtecture] = struct{}{}
+
+					t.Run(img.Archtecture, func(t *testing.T) {
+						ctx := zlog.Test(ctx, t)
+						m, err := ar.Manifest(img.ID)
+						if err != nil {
+							t.Fatal(err)
+						}
+
+						// This is the top-most layer.
+						digest := img.ParsedData.Layers[len(img.ParsedData.Layers)-1]
+						layerRef := test.LayerRef{
+							Registry: reg,
+							Name:     repo,
+							Digest:   digest,
+						}
+
+						// Fetch the layer via the test caching machinery.
+						a.LoadLayerFromRegistry(ctx, t, layerRef)
+						r := a.Realizer(ctx).(*test.CachedRealizer)
+						t.Cleanup(func() {
+							if err := r.Close(); err != nil {
+								t.Error(err)
+							}
+						})
+						ls, err := r.RealizeDescriptions(ctx, []claircore.LayerDescription{
+							{
+								Digest:    digest,
+								URI:       "http://example.com",
+								MediaType: test.MediaType,
+								Headers:   make(map[string][]string),
+							},
+						})
+						if err != nil {
+							t.Fatal(err)
+						}
+
+						got, err := new(Scanner).Scan(ctx, &ls[0])
+						if err != nil {
+							t.Error(err)
+						}
+						seq := rpmtest.PackagesFromManifest(t, slices.Values(m.RPMs))
+						want := wart.CollectPointer(seq)
+
+						opts := rpmtest.Options(t)
+						if !cmp.Equal(got, want, opts) {
+							t.Error(cmp.Diff(got, want, opts))
+						}
+					})
+				}
+			})
 		}
 	}
 }
