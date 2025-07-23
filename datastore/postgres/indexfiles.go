f9e63b5fdc6324d930ff882e186f596cbc3d3358
Hank Donnay <hdonnay@redhat.com>
2025-07-23 10:02:24 -0500

    postgres: change batching for Files
    Signed-off-by: Hank Donnay <hdonnay@redhat.com>

--- datastore/postgres/indexfiles.go
+++ datastore/postgres/indexfiles.go
@@ -5,12 +5,12 @@ import (
 	"fmt"
 	"time"
 
+	"github.com/jackc/pgx/v5"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promauto"
 
 	"github.com/quay/claircore"
 	"github.com/quay/claircore/indexer"
-	"github.com/quay/claircore/pkg/microbatch"
 )
 
 var (
@@ -65,89 +65,58 @@ func (s *IndexerStore) IndexFiles(ctx context.Context, files []claircore.File, l
 		`
 	)
 
-	var layerID, scannerID int64
+	err := pgx.BeginFunc(ctx, s.pool, func(tx pgx.Tx) error {
+		var layerID, scannerID int64
+		var batch pgx.Batch
+		var start time.Time
+		var err error
 
-	tx, err := s.pool.Begin(ctx)
-	if err != nil {
-		return fmt.Errorf("failed to create transaction: %v", err)
-	}
-	defer tx.Rollback(ctx)
-
-	// Get layerID
-	start := time.Now()
-	layerRow := tx.QueryRow(ctx, lookupLayerID, layer.Hash)
-	err = layerRow.Scan(&layerID)
-	if err != nil {
-		return fmt.Errorf("failed look up layer ID: %v", err)
-	}
-	indexFilesCounter.WithLabelValues("lookup_layer").Add(1)
-	indexFilesDuration.WithLabelValues("lookup_layer").Observe(time.Since(start).Seconds())
-
-	// Get scannerID
-	start = time.Now()
-	scannerRow := tx.QueryRow(ctx, lookupScannerID, scnr.Name(), scnr.Version(), scnr.Kind())
-	err = scannerRow.Scan(&scannerID)
-	if err != nil {
-		return fmt.Errorf("failed look up scanner ID: %v", err)
-	}
-	indexFilesCounter.WithLabelValues("lookup_scanner").Add(1)
-	indexFilesDuration.WithLabelValues("lookup_scanner").Observe(time.Since(start).Seconds())
-
-	insertFileStmt, err := tx.Prepare(ctx, "insertFileStmt", insert)
-	if err != nil {
-		return fmt.Errorf("failed to create statement: %w", err)
-	}
-	insertFileScanArtifactWithStmt, err := tx.Prepare(ctx, "insertFileScanArtifactWithStmt", insertWith)
-	if err != nil {
-		return fmt.Errorf("failed to create statement: %w", err)
-	}
-
-	start = time.Now()
-	mBatcher := microbatch.NewInsert(tx, 500, time.Minute)
-	for _, f := range files {
-		err := mBatcher.Queue(
-			ctx,
-			insertFileStmt.SQL,
-			f.Path,
-			f.Kind,
-		)
+		start = time.Now()
+		err = tx.QueryRow(ctx, lookupLayerID, layer.Hash).Scan(&layerID)
 		if err != nil {
-			return fmt.Errorf("batch insert failed for file %v: %w", f, err)
+			return fmt.Errorf("failed look up layer ID: %v", err)
 		}
-	}
-	err = mBatcher.Done(ctx)
-	if err != nil {
-		return fmt.Errorf("final batch insert failed for file: %w", err)
-	}
-	indexFilesCounter.WithLabelValues("insert_batch").Add(1)
-	indexFilesDuration.WithLabelValues("insert_batch").Observe(time.Since(start).Seconds())
+		indexFilesCounter.WithLabelValues("lookup_layer").Add(1)
+		indexFilesDuration.WithLabelValues("lookup_layer").Observe(time.Since(start).Seconds())
 
-	// make file scan artifacts
-	start = time.Now()
-	mBatcher = microbatch.NewInsert(tx, 500, time.Minute)
-	for _, f := range files {
-		err := mBatcher.Queue(
-			ctx,
-			insertFileScanArtifactWithStmt.SQL,
-			f.Path,
-			f.Kind,
-			layerID,
-			scannerID,
-		)
+		start = time.Now()
+		err = tx.QueryRow(ctx, lookupScannerID, scnr.Name(), scnr.Version(), scnr.Kind()).Scan(&scannerID)
 		if err != nil {
-			return fmt.Errorf("batch insert failed for file_scanartifact %v: %w", f, err)
+			return fmt.Errorf("failed look up scanner ID: %v", err)
 		}
-	}
-	err = mBatcher.Done(ctx)
-	if err != nil {
-		return fmt.Errorf("final batch insert failed for file_scanartifact: %w", err)
-	}
-	indexFilesCounter.WithLabelValues("insertWith_batch").Add(1)
-	indexFilesDuration.WithLabelValues("insertWith_batch").Observe(time.Since(start).Seconds())
+		indexFilesCounter.WithLabelValues("lookup_scanner").Add(1)
+		indexFilesDuration.WithLabelValues("lookup_scanner").Observe(time.Since(start).Seconds())
 
-	err = tx.Commit(ctx)
+		start = time.Now()
+		for _, f := range files {
+			batch.Queue(insert, f.Path, f.Kind)
+		}
+		err = tx.SendBatch(ctx, &batch).Close()
+		if err != nil {
+			return fmt.Errorf("batch insert failed for file: %w", err)
+		}
+		indexFilesCounter.WithLabelValues("insert_batch").Add(1)
+		indexFilesDuration.WithLabelValues("insert_batch").Observe(time.Since(start).Seconds())
+
+		clear(batch.QueuedQueries)
+		batch.QueuedQueries = batch.QueuedQueries[:0]
+
+		start = time.Now()
+		for _, f := range files {
+			batch.Queue(insertWith, f.Path, f.Kind, layerID, scannerID)
+		}
+		err = tx.SendBatch(ctx, &batch).Close()
+		if err != nil {
+			return fmt.Errorf("batch insert failed for file_scanartifact: %w", err)
+		}
+		indexFilesCounter.WithLabelValues("insertWith_batch").Add(1)
+		indexFilesDuration.WithLabelValues("insertWith_batch").Observe(time.Since(start).Seconds())
+
+		return nil
+	})
 	if err != nil {
-		return fmt.Errorf("failed to commit tx: %w", err)
+		return fmt.Errorf("IndexFiles failed: %w", err)
 	}
+
 	return nil
 }
