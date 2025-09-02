426b0511692e407f6a9957e242e92bb20d15615a
Hank Donnay <hdonnay@redhat.com>
2025-09-02 14:03:42 -0500

    httputil: modernize: slicescontains
    Signed-off-by: Hank Donnay <hdonnay@redhat.com>

--- internal/httputil/responsechecker.go
+++ internal/httputil/responsechecker.go
@@ -4,19 +4,14 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"slices"
 )
 
 // CheckResponse takes a http.Response and a variadic of ints representing
 // acceptable http status codes. The error returned will attempt to include
 // some content from the server's response.
 func CheckResponse(resp *http.Response, acceptableCodes ...int) error {
-	acceptable := false
-	for _, code := range acceptableCodes {
-		if resp.StatusCode == code {
-			acceptable = true
-			break
-		}
-	}
+	acceptable := slices.Contains(acceptableCodes, resp.StatusCode)
 	if !acceptable {
 		limitBody, err := io.ReadAll(io.LimitReader(resp.Body, 256))
 		if err == nil {
