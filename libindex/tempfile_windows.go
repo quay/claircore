9f67cbdfbde1f51016cf197f07bb27a9a27098ce
Hank Donnay <hdonnay@redhat.com>
2025-09-05 15:17:57 -0500

    libindex: undo linker tricks on windows
    Since this code was added, the `math/rand/v2` package has landed and
    does the same thing (use the runtime's fast random numbers), but in a
    supported way.
    
    Signed-off-by: Hank Donnay <hdonnay@redhat.com>

--- libindex/tempfile_windows.go
+++ libindex/tempfile_windows.go
@@ -4,16 +4,13 @@ import (
 	"errors"
 	"fmt"
 	"io/fs"
+	"math/rand/v2"
 	"os"
 	"path/filepath"
-	_ "unsafe" // linker tricks
 )
 
 // This is a very rough port of src/os/tempfile.go that adds the magic
 // autodelete flag.
-
-//go:linkname fastrand runtime.fastrand
-func fastrand() uint32
 
 type tempFile struct {
 	*os.File
@@ -23,7 +20,7 @@ func openTemp(dir string) (*tempFile, error) {
 	// Copied out of golang.org/x/sys/windows
 	const FILE_FLAG_DELETE_ON_CLOSE = 0x04000000
 	for {
-		fn := fmt.Sprintf("fetch.%d", fastrand())
+		fn := fmt.Sprintf("fetch.%08x", rand.Uint32())
 		f, err := os.OpenFile(filepath.Join(dir, fn), os.O_WRONLY|FILE_FLAG_DELETE_ON_CLOSE, 0644)
 		switch {
 		case errors.Is(err, nil):
