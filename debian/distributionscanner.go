185c9cef88d48af151bd16c9cb77f64c491c3c70
Hank Donnay <hdonnay@redhat.com>
2025-10-02 10:28:44 -0500

    debian: migrate to slog
    Signed-off-by: Hank Donnay <hdonnay@redhat.com>

--- debian/distributionscanner.go
+++ debian/distributionscanner.go
@@ -5,13 +5,12 @@ import (
 	"errors"
 	"fmt"
 	"io/fs"
+	"log/slog"
 	"regexp"
 	"runtime/trace"
 	"strconv"
 	"strings"
 	"unicode"
-
-	"github.com/quay/zlog"
 
 	"github.com/quay/claircore"
 	"github.com/quay/claircore/indexer"
@@ -39,18 +38,15 @@ func (*DistributionScanner) Kind() string { return "distribution" }
 // Scan implements [indexer.DistributionScanner].
 func (ds *DistributionScanner) Scan(ctx context.Context, l *claircore.Layer) ([]*claircore.Distribution, error) {
 	defer trace.StartRegion(ctx, "Scanner.Scan").End()
-	ctx = zlog.ContextWithValues(ctx,
-		"component", "debian/DistributionScanner.Scan",
-		"version", ds.Version(),
-		"layer", l.Hash.String())
-	zlog.Debug(ctx).Msg("start")
-	defer zlog.Debug(ctx).Msg("done")
+	log := slog.With("version", ds.Version(), "layer", l.Hash.String())
+	log.DebugContext(ctx, "start")
+	defer log.DebugContext(ctx, "done")
 
 	sys, err := l.FS()
 	if err != nil {
 		return nil, fmt.Errorf("debian: unable to open layer: %w", err)
 	}
-	d, err := findDist(ctx, sys)
+	d, err := findDist(ctx, log, sys)
 	if err != nil {
 		return nil, err
 	}
@@ -60,20 +56,19 @@ func (ds *DistributionScanner) Scan(ctx context.Context, l *claircore.Layer) ([]
 	return []*claircore.Distribution{d}, nil
 }
 
-func findDist(ctx context.Context, sys fs.FS) (*claircore.Distribution, error) {
+func findDist(ctx context.Context, log *slog.Logger, sys fs.FS) (*claircore.Distribution, error) {
 	f, err := sys.Open(osrelease.Path)
 	switch {
 	case errors.Is(err, nil):
 	case errors.Is(err, fs.ErrNotExist):
-		zlog.Debug(ctx).Msg("no os-release file")
+		log.DebugContext(ctx, "no os-release file")
 		return nil, nil
 	default:
 		return nil, fmt.Errorf("debian: unexpected error: %w", err)
 	}
 	kv, err := osrelease.Parse(ctx, f)
 	if err != nil {
-		zlog.Info(ctx).
-			Err(err).Msg("malformed os-release file")
+		log.InfoContext(ctx, "malformed os-release file", "reason", err)
 		return nil, nil
 	}
 	if kv[`ID`] != `debian` {
@@ -89,14 +84,12 @@ func findDist(ctx context.Context, sys fs.FS) (*claircore.Distribution, error) {
 		name = strings.TrimFunc(ver.FindString(kv[`VERSION`]), func(r rune) bool { return !unicode.IsLetter(r) })
 	}
 	if name == "" || idstr == "" {
-		zlog.Info(ctx).
-			Err(err).Msg("malformed os-release file")
+		log.InfoContext(ctx, "malformed os-release file", "reason", err)
 		return nil, nil
 	}
 	id, err := strconv.ParseInt(idstr, 10, 32)
 	if err != nil {
-		zlog.Info(ctx).
-			Err(err).Msg("malformed os-release file")
+		log.InfoContext(ctx, "malformed os-release file", "reason", err)
 		return nil, nil
 	}
 	return mkDist(name, int(id)), nil
