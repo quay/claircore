198e8bc3b179e917923c04c4dd525230e8faea72
Hank Donnay <hdonnay@redhat.com>
2025-10-06 12:58:05 -0500

    docs: migrate to slog
    Signed-off-by: Hank Donnay <hdonnay@redhat.com>

--- docs/logger_test.go
+++ docs/logger_test.go
@@ -2,26 +2,35 @@ package docs_test
 
 import (
 	"context"
+	"fmt"
+	"log/slog"
 	"time"
 
-	"github.com/quay/zlog"
+	"github.com/quay/claircore/toolkit/log"
 )
 
 // Example_logger is an example annotated for inclusion in the prose
 // documentation.
 func Example_logger() {
 	ctx := context.Background()
-	// ANCHOR: kvs
-	ctx = zlog.ContextWithValues(ctx,
-		"component", "Example.Logger")
-	// ANCHOR_END: kvs
+	{
+		// ANCHOR: kvs
+		log := slog.With("contextual", "value")
+		log.DebugContext(ctx, "message")
+		// ANCHOR_END: kvs
+	}
+
+	{
+		// ANCHOR: ctx
+		ctx := log.With(ctx, "contextual", "value")
+		slog.DebugContext(ctx, "message")
+		// ANCHOR_END: ctx
+	}
 
 	// ANCHOR: bad_example
-	zlog.Info(ctx).Msgf("done at: %v", time.Now())
+	slog.InfoContext(ctx, fmt.Sprintf("done at: %v", time.Now()))
 	// ANCHOR_END: bad_example
 	// ANCHOR: good_example
-	zlog.Info(ctx).
-		Time("time", time.Now()).
-		Msgf("done")
+	slog.InfoContext(ctx, "done", "time", time.Now())
 	// ANCHOR_END: good_example
 }
