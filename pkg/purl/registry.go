82f87008a8ec968ba47580c7e8b68283ab5df2fc
crozzy <joseph.crosland@gmail.com>
2025-10-24 15:29:07 -0700

    purl: add purl registry
    This change adds the machinary needed to register PURL parsing and
    generating functions from the individual ecosystems.
    
    Signed-off-by: crozzy <joseph.crosland@gmail.com>

--- pkg/purl/registry.go
+++ pkg/purl/registry.go
@@ -20,17 +20,17 @@ type PURLRegistry interface {
 	Parse(ctx context.Context, purl packageurl.PackageURL) (*claircore.IndexRecord, error)
 }
 
-// ErrUnPurlable is returned when a PURL generator is not registered for a scanner.
-type ErrUnPurlable struct{ ScannerName string }
+// ErrUnPurlable is returned when a PURL generator is not registered for a detector.
+type ErrUnPurlable struct{ DetectorName string }
 
 // Error returns the error message.
 func (e ErrUnPurlable) Error() string {
-	return fmt.Sprintf("no PURL generator registered for scanner %q", e.ScannerName)
+	return fmt.Sprintf("no PURL generator registered for scanner %q", e.DetectorName)
 }
 
 // NewErrUnPurlable creates a new ErrUnPurlable.
-func NewErrUnPurlable(scannerName string) ErrUnPurlable {
-	return ErrUnPurlable{ScannerName: scannerName}
+func NewErrUnPurlable(detectorName string) ErrUnPurlable {
+	return ErrUnPurlable{DetectorName: detectorName}
 }
 
 // ErrUnknownPurl is returned when a PURL parser is not registered for a PURL type.
@@ -50,31 +50,31 @@ func NewErrUnknownPurl(purl packageurl.PackageURL) ErrUnknownPurl {
 }
 
 // GenerateFunc produces a PackageURL for a given IndexRecord.
-// Implementations should be deterministic and side-effect free.
 type GenerateFunc func(ctx context.Context, ir *claircore.IndexRecord) (packageurl.PackageURL, error)
 
-// ParseFunc produces an IndexRecord for a given PackageURL.
-// Implementations should be deterministic and side-effect free.
-type ParseFunc func(ctx context.Context, purl packageurl.PackageURL) (*claircore.IndexRecord, error)
+// ParseFunc produces IndexRecords for a given PackageURL.
+type ParseFunc func(ctx context.Context, purl packageurl.PackageURL) ([]*claircore.IndexRecord, error)
 
 // Registry is a thread-safe registry of PURL generators and parsers.
 type Registry struct {
-	genRegistry   map[string]GenerateFunc
-	parseRegistry map[string]ParseFunc
-	mu            sync.RWMutex
+	genRegistry       map[string]GenerateFunc
+	parseRegistry     map[string]ParseFunc
+	transformRegistry map[string][]TransformerFunc
+	mu                sync.RWMutex
 }
 
 // NewRegistry creates a new PURLRegistry.
 func NewRegistry() *Registry {
 	return &Registry{
-		genRegistry:   make(map[string]GenerateFunc),
-		parseRegistry: make(map[string]ParseFunc),
-		mu:            sync.RWMutex{},
+		genRegistry:       make(map[string]GenerateFunc),
+		parseRegistry:     make(map[string]ParseFunc),
+		transformRegistry: make(map[string][]TransformerFunc),
+		mu:                sync.RWMutex{},
 	}
 }
 
-// Generate finds a registered generator by the record's scanner name and
-// returns the generated PackageURL.
+// Generate finds a registered generator by the package's [claircore.Detector.Name]
+// and returns the generated PackageURL.
 func (r *Registry) Generate(ctx context.Context, ir *claircore.IndexRecord) (packageurl.PackageURL, error) {
 	if ir.Package == nil || ir.Package.Detector == nil || ir.Package.Detector.Name == "" {
 		return packageurl.PackageURL{}, NewErrUnPurlable("unknown")
@@ -88,13 +88,27 @@ func (r *Registry) Generate(ctx context.Context, ir *claircore.IndexRecord) (pac
 	return f(ctx, ir)
 }
 
-// Parse finds a registered parser by the record's scanner name and
-// returns the generated PackageURL.
-func (r *Registry) Parse(ctx context.Context, purl packageurl.PackageURL) (*claircore.IndexRecord, error) {
-	r.mu.RLock()
+// Parse finds a registered parser by the PURL's namespace and type and returns
+// IndexRecords.
+func (r *Registry) Parse(ctx context.Context, purl packageurl.PackageURL) ([]*claircore.IndexRecord, error) {
 	if purl.Namespace == "" {
 		purl.Namespace = NoneNamespace
 	}
+
+	r.mu.RLock()
+	transformFuncs, ok := r.transformRegistry[createPurlKey(purl.Type, purl.Namespace)]
+	r.mu.RUnlock()
+	// Unlocked during transform functions to avoid locking the registry
+	// while external code is executing.
+	if ok {
+		for _, tf := range transformFuncs {
+			err := tf(ctx, &purl)
+			if err != nil {
+				return nil, fmt.Errorf("purl transform error: %w", err)
+			}
+		}
+	}
+	r.mu.RLock()
 	f, ok := r.parseRegistry[createPurlKey(purl.Type, purl.Namespace)]
 	r.mu.RUnlock()
 	if !ok {
@@ -103,17 +117,22 @@ func (r *Registry) Parse(ctx context.Context, purl packageurl.PackageURL) (*clai
 	return f(ctx, purl)
 }
 
-// RegisterScanner registers using an indexer.PackageScanner (for its Name).
-func (r *Registry) RegisterScanner(s indexer.PackageScanner, GenFn GenerateFunc) {
+// RegisterDetector registers using an indexer.PackageScanner (for its Name).
+func (r *Registry) RegisterDetector(s indexer.PackageScanner, GenFn GenerateFunc) {
 	r.mu.Lock()
 	r.genRegistry[s.Name()] = GenFn
 	r.mu.Unlock()
 }
 
-// RegisterPurlType registers using a purl type.
-func (r *Registry) RegisterPurlType(purlType string, purlNamespace string, ParseFn ParseFunc) {
+// TransformerFunc transforms a PackageURL before it is parsed.
+type TransformerFunc func(ctx context.Context, purl *packageurl.PackageURL) error
+
+// RegisterPurlType registers using a purl type. Transform functions are applied in
+// registration order to the PackageURL before it is parsed.
+func (r *Registry) RegisterPurlType(purlType string, purlNamespace string, ParseFn ParseFunc, transformFuncs ...TransformerFunc) {
 	r.mu.Lock()
 	r.parseRegistry[createPurlKey(purlType, purlNamespace)] = ParseFn
+	r.transformRegistry[createPurlKey(purlType, purlNamespace)] = transformFuncs
 	r.mu.Unlock()
 }
 
