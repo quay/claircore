63272f048fb52cfe4462438c6b71fd588c08e467
crozzy <joseph.crosland@gmail.com>
2025-11-12 08:31:21 -0800

    rhcc: add purl generator and parser
    Add GeneratePURL and ParsePURL to translate from IndexRecord to PURL and
    back.
    
    Signed-off-by: crozzy <joseph.crosland@gmail.com>

---
+++ rhel/rhcc/purl.go
@@ -0,0 +1,80 @@
+package rhcc
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/Masterminds/semver"
+	"github.com/package-url/packageurl-go"
+
+	"github.com/quay/claircore"
+	"github.com/quay/claircore/toolkit/types/cpe"
+)
+
+const (
+	// PURLType is the type of package URL for Red Hat Container Catalog packages.
+	PURLType = "oci"
+)
+
+// GenerateOCIPURL generates an OCI PURL for a given [claircore.IndexRecord].
+// Example:
+// pkg:oci/ubi@sha256:dbc1e98d14a022542e45b5f22e0206d3f86b5bdf237b58ee7170c9ddd1b3a283?repository_url=registry.access.redhat.com/ubi9/ubi
+func GenerateOCIPURL(ctx context.Context, ir *claircore.IndexRecord) (packageurl.PackageURL, error) {
+	purl := packageurl.PackageURL{
+		Type: PURLType,
+		Name: ir.Package.Name,
+		// This tends to be represented as a digest but we don't persist that information
+		// or use it for matching so we use the version as it is the most applicable.
+		Version: ir.Package.Version,
+		Qualifiers: packageurl.QualifiersFromMap(map[string]string{
+			"arch": ir.Package.Arch,
+			"tag":  ir.Package.Version,
+		}),
+	}
+	if ir.Repository != nil && ir.Repository.Name != GoldRepo.Name {
+		purl.Qualifiers = append(
+			purl.Qualifiers,
+			packageurl.Qualifier{Key: "container_cpe", Value: ir.Repository.CPE.String()},
+		)
+	}
+	return purl, nil
+}
+
+// ParseOCIPURL parses an OCI PURL into a list of [claircore.IndexRecord]s.
+// The matcher needs the NormalizedVersion to be set.
+func ParseOCIPURL(ctx context.Context, purl packageurl.PackageURL) ([]*claircore.IndexRecord, error) {
+	ir := &claircore.IndexRecord{
+		Package: &claircore.Package{
+			Name:           purl.Name,
+			Version:        purl.Version,
+			Arch:           purl.Qualifiers.Map()["arch"],
+			RepositoryHint: "rhcc",
+		},
+	}
+
+	ir.Repository = &GoldRepo
+	if containerCPE, ok := purl.Qualifiers.Map()["container_cpe"]; ok {
+		cpe, err := cpe.Unbind(containerCPE)
+		if err != nil {
+			return nil, err
+		}
+		ir.Repository = &claircore.Repository{
+			CPE:  cpe,
+			Name: cpe.String(),
+			Key:  RepositoryKey,
+		}
+	}
+
+	// Deal with the version.
+	ir.Package.Version = purl.Version
+	if tag := purl.Qualifiers.Map()["tag"]; tag != "" {
+		// Prefer the tag over the version if it is present.
+		ir.Package.Version = tag
+	}
+	sv, err := semver.NewVersion(ir.Package.Version)
+	if err != nil {
+		return nil, fmt.Errorf("error parsing version: %w", err)
+	}
+	ir.Package.NormalizedVersion = claircore.FromSemver(sv)
+	return []*claircore.IndexRecord{ir}, nil
+}
---
+++ rhel/rhcc/purl_test.go
@@ -0,0 +1,153 @@
+package rhcc
+
+import (
+	"context"
+	"testing"
+
+	"github.com/Masterminds/semver"
+	"github.com/google/go-cmp/cmp"
+	"github.com/google/go-cmp/cmp/cmpopts"
+	"github.com/package-url/packageurl-go"
+
+	"github.com/quay/claircore"
+	"github.com/quay/claircore/toolkit/types/cpe"
+)
+
+func TestRoundTripIndexRecordOCI(t *testing.T) {
+	t.Parallel()
+	ctx := context.Background()
+
+	tests := []struct {
+		name string
+		ir   *claircore.IndexRecord
+	}{
+		{
+			name: "goldrepo-no-container-cpe",
+			ir: &claircore.IndexRecord{
+				Package: &claircore.Package{
+					Name:              "ubi",
+					Version:           "v9.3.1",
+					NormalizedVersion: claircore.FromSemver(semver.MustParse("v9.3.1")),
+					Arch:              "x86_64",
+					RepositoryHint:    "rhcc",
+				},
+				Repository: &GoldRepo,
+			},
+		},
+		{
+			name: "with-container-cpe",
+			ir: &claircore.IndexRecord{
+				Package: &claircore.Package{
+					Name:              "ubi-micro",
+					Version:           "v9.3.1",
+					NormalizedVersion: claircore.FromSemver(semver.MustParse("v9.3.1")),
+					Arch:              "x86_64",
+					RepositoryHint:    "rhcc",
+				},
+				Repository: &claircore.Repository{
+					CPE:  cpe.MustUnbind("cpe:/o:redhat:enterprise_linux:9::baseos"),
+					Name: "cpe:2.3:o:redhat:enterprise_linux:9:*:baseos:*:*:*:*:*",
+					Key:  RepositoryKey,
+				},
+			},
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+
+			p, err := GenerateOCIPURL(ctx, tc.ir)
+			if err != nil {
+				t.Fatalf("GenerateOCIPURL: %v", err)
+			}
+			t.Logf("generated PURL: %s", p.String())
+			got, err := ParseOCIPURL(ctx, p)
+			if err != nil {
+				t.Fatalf("ParseOCIPURL: %v", err)
+			}
+			if diff := cmp.Diff(got, []*claircore.IndexRecord{tc.ir}, purlCmp); diff != "" {
+				t.Fatalf("round-trip mismatch (-got +want):\n%s", diff)
+			}
+		})
+	}
+}
+
+func TestGenerateOCIPURL(t *testing.T) {
+	t.Parallel()
+	ctx := context.Background()
+	tests := []struct {
+		name string
+		ir   *claircore.IndexRecord
+		want packageurl.PackageURL
+	}{
+		{
+			name: "basic-goldrepo",
+			ir: &claircore.IndexRecord{
+				Package: &claircore.Package{
+					Name:    "ubi",
+					Version: "v9.3.1",
+					Arch:    "amd64",
+				},
+				Repository: &GoldRepo,
+			},
+			want: packageurl.PackageURL{
+				Type:    PURLType,
+				Name:    "ubi",
+				Version: "v9.3.1",
+				Qualifiers: packageurl.Qualifiers{
+					{Key: "arch", Value: "amd64"},
+					{Key: "tag", Value: "v9.3.1"},
+				},
+			},
+		},
+		{
+			name: "with-container-cpe",
+			ir: &claircore.IndexRecord{
+				Package: &claircore.Package{
+					Name:    "ubi",
+					Version: "v9.3.1",
+					Arch:    "amd64",
+				},
+				Repository: &claircore.Repository{
+					CPE:  cpe.MustUnbind("cpe:/o:redhat:enterprise_linux:9::baseos"),
+					Name: "cpe:2.3:o:redhat:enterprise_linux:9:*:baseos:*:*:*:*:*",
+					Key:  RepositoryKey,
+				},
+			},
+			want: packageurl.PackageURL{
+				Type:    PURLType,
+				Name:    "ubi",
+				Version: "v9.3.1",
+				Qualifiers: packageurl.Qualifiers{
+					{Key: "arch", Value: "amd64"},
+					{Key: "tag", Value: "v9.3.1"},
+					{Key: "container_cpe", Value: "cpe:2.3:o:redhat:enterprise_linux:9:*:baseos:*:*:*:*:*"},
+				},
+			},
+		},
+	}
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			got, err := GenerateOCIPURL(ctx, tc.ir)
+			if err != nil {
+				t.Fatalf("GenerateOCIPURL: %v", err)
+			}
+			if diff := cmp.Diff(got, tc.want); diff != "" {
+				t.Fatalf("purl mismatch (-got +want):\n%s", diff)
+			}
+		})
+	}
+}
+
+var purlCmp = cmp.Options{
+	// Ignore Distribution field as there isn't currently a serialized format
+	// and it is not currently used in the matching.
+	cmpopts.IgnoreFields(claircore.IndexRecord{}, "Distribution"),
+	cmpCPE,
+}
+
+var cmpCPE = cmp.FilterPath(
+	func(p cmp.Path) bool { return p.Last().String() == ".CPE" },
+	cmp.Comparer(func(a, b cpe.WFN) bool { return a.String() == b.String() }),
+)
