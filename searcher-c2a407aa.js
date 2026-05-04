643101a0c6cd2413dc6d2cd2279cec568f45d447
hdonnay <hdonnay@users.noreply.github.com>
2026-05-04 15:30:05 +0000

    deploy: 1eada2e508138813ff7547eb09c43752280cdb9c

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-4aae1b67.js',
+                path_to_root + 'searchindex-6e4f8990.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
