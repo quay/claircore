e30f49556626bb9c9f2c35ee108fc0995a4c08ce
Hank Donnay <hdonnay@redhat.com>
2026-05-08 13:21:01 -0500

    vex: consolodate document parsing logic
    Signed-off-by: Hank Donnay <hdonnay@redhat.com>
    Change-Id: I1d5aaae7af6a970b743e70c61ef466e36a6a6964

---
+++ rhel/vex/doc.go
@@ -0,0 +1,5 @@
+// Package vex implements [Red Hat specific interpretation] of [VEX] documents.
+//
+// [Red Hat specific interpretation]: https://redhatproductsecurity.github.io/security-data-guidelines/csaf-vex/
+// [VEX]: https://docs.oasis-open.org/csaf/csaf/v2.0/os/csaf-v2.0-os.html
+package vex
