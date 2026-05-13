103a69db3e8e87c690085a49775d416ace247fca
Brad Lugo <blugo@redhat.com>
2026-05-13 12:31:28 -0700

    rhcc: fix matching for timestamp-based versions
    rpmver.Parse requires a string in the EVR format (version-release, with
    at least one dash). For newer (e.g., Konflux-built) Red Hat images, the
    rhcc detector parses a Unix timestamp-based version (e.g., "1744596866")
    from the labels.json file and the VEX parser extracts matching timestamp
    tags from OCI PURLs, neither of which contain a dash. This caused
    rpmver.Parse to reject them with "missing separators", preventing any
    vulnerability matching for containers using the new labels.json
    metadata.
    
    These changes normalize version strings in the matcher by appending "-0"
    when no dash (release) is present, consistent with what rhctag.toEVR()
    already does.
    
    Signed-off-by: Brad Lugo <blugo@redhat.com>

---
+++ rhel/rhcc/matcher_test.go
@@ -0,0 +1,106 @@
+package rhcc
+
+import (
+	"testing"
+
+	"github.com/quay/claircore"
+	"github.com/quay/claircore/test"
+	"github.com/quay/claircore/toolkit/types/cpe"
+)
+
+func TestVulnerable(t *testing.T) {
+	t.Parallel()
+
+	type testcase struct {
+		name           string
+		packageVersion string
+		fixedInVersion string
+		repoCPE        cpe.WFN
+		vulnRepoCPE    cpe.WFN
+		want           bool
+	}
+	table := []testcase{
+		{
+			name:           "TimestampOlder",
+			packageVersion: "1740000000",
+			fixedInVersion: "1742843776",
+			repoCPE:        cpe.MustUnbind("cpe:/a:redhat:openshift_gitops:1.16::el8"),
+			vulnRepoCPE:    cpe.MustUnbind("cpe:/a:redhat:openshift_gitops:1.16::el8"),
+			want:           true,
+		},
+		{
+			name:           "TimestampNewer",
+			packageVersion: "1744596866",
+			fixedInVersion: "1742843776",
+			repoCPE:        cpe.MustUnbind("cpe:/a:redhat:openshift_gitops:1.16::el8"),
+			vulnRepoCPE:    cpe.MustUnbind("cpe:/a:redhat:openshift_gitops:1.16::el8"),
+			want:           false,
+		},
+		{
+			name:           "TimestampEqual",
+			packageVersion: "1742843776",
+			fixedInVersion: "1742843776",
+			repoCPE:        cpe.MustUnbind("cpe:/a:redhat:openshift_gitops:1.16::el8"),
+			vulnRepoCPE:    cpe.MustUnbind("cpe:/a:redhat:openshift_gitops:1.16::el8"),
+			want:           false,
+		},
+		{
+			name:           "TagOlder",
+			packageVersion: "v3.5.5-4",
+			fixedInVersion: "v3.5.7-8",
+			repoCPE:        cpe.MustUnbind("cpe:/a:redhat:quay:3::el8"),
+			vulnRepoCPE:    cpe.MustUnbind("cpe:/a:redhat:quay:3::el8"),
+			want:           true,
+		},
+		{
+			name:           "TagNewer",
+			packageVersion: "v3.5.9-2",
+			fixedInVersion: "v3.5.7-8",
+			repoCPE:        cpe.MustUnbind("cpe:/a:redhat:quay:3::el8"),
+			vulnRepoCPE:    cpe.MustUnbind("cpe:/a:redhat:quay:3::el8"),
+			want:           false,
+		},
+		{
+			name:           "TagCPEMismatch",
+			packageVersion: "v3.5.5-4",
+			fixedInVersion: "v3.5.7-8",
+			repoCPE:        cpe.MustUnbind("cpe:/a:redhat:quay:3::el8"),
+			vulnRepoCPE:    cpe.MustUnbind("cpe:/a:redhat:openshift:4::el8"),
+			want:           false,
+		},
+	}
+
+	var m matcher
+	ctx := test.Logging(t)
+	for _, tc := range table {
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+			record := &claircore.IndexRecord{
+				Package: &claircore.Package{
+					Version: tc.packageVersion,
+				},
+				Repository: &claircore.Repository{
+					Key:  RepositoryKey,
+					Name: tc.repoCPE.String(),
+					CPE:  tc.repoCPE,
+				},
+			}
+			vuln := &claircore.Vulnerability{
+				FixedInVersion: tc.fixedInVersion,
+				Repo: &claircore.Repository{
+					Key:  RepositoryKey,
+					Name: tc.vulnRepoCPE.String(),
+					CPE:  tc.vulnRepoCPE,
+				},
+			}
+			got, err := m.Vulnerable(ctx, record, vuln)
+			if err != nil {
+				t.Error(err)
+			}
+			if got != tc.want {
+				t.Errorf("%q failed: Vulnerable(%q, %q) = %v, want %v",
+					tc.name, tc.packageVersion, tc.fixedInVersion, got, tc.want)
+			}
+		})
+	}
+}
