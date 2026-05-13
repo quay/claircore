439c3ce73d05b04659b5725de609ff8c8dce6e32
Brad Lugo <blugo@redhat.com>
2026-05-13 12:31:28 -0700

    rhcc: fix matching for timestamp-based versions
    rpmver.Parse requires a string in the EVR format (version-release, with
    at least one dash). For newer (e.g., Konflux-built) Red Hat images, the
    rhcc detector parses a Unix timestamp-based version (e.g., "1744596866")
    from the labels.json file and the VEX parser extracts matching timestamp
    tags from OCI PURLs, neither of which contain a dash. This caused
    rpmver.Parse to reject them with "missing separators", preventing any
    vulnerability matching for containers using the new labels.json
    metadata.
    
    These changes normalize version strings in the matcher by appending "-0"
    when no dash (release) is present, consistent with what rhctag.toEVR()
    already does.
    
    Signed-off-by: Brad Lugo <blugo@redhat.com>

--- rhel/rhcc/matcher_test.go
+++ rhel/rhcc/matcher_test.go
@@ -5,67 +5,129 @@ import (
 
 	"github.com/quay/claircore"
 	"github.com/quay/claircore/test"
+	"github.com/quay/claircore/toolkit/types/cpe"
 )
 
 func TestMatcherVulnerable(t *testing.T) {
 	t.Parallel()
 	ctx := test.Logging(t)
-	m := &matcher{}
 
-	t.Run("Inverted", func(t *testing.T) {
-		record := &claircore.IndexRecord{
-			Package:    &claircore.Package{Name: "mta/mta-rhel8-operator", Version: "7.0.3-13"},
-			Repository: &GoldRepo,
-		}
-		vuln := &claircore.Vulnerability{
-			Name:   "CVE-2024-24786",
-			Invert: true,
-			Repo:   &GoldRepo,
-		}
-		got, err := m.Vulnerable(ctx, record, vuln)
-		if err != nil {
-			t.Fatal(err)
-		}
-		if !got {
-			t.Error("expected vulnerable=true for inverted vulnerability")
-		}
-	})
+	type testcase struct {
+		name           string
+		packageVersion string
+		fixedInVersion string
+		repo           *claircore.Repository
+		vulnRepo       *claircore.Repository
+		invert         bool
+		want           bool
+	}
+	table := []testcase{
+		{
+			name:           "TimestampOlder",
+			packageVersion: "1740000000",
+			fixedInVersion: "1742843776",
+			repo:           cpeRepo("cpe:/a:redhat:openshift_gitops:1.16::el8"),
+			vulnRepo:       cpeRepo("cpe:/a:redhat:openshift_gitops:1.16::el8"),
+			want:           true,
+		},
+		{
+			name:           "TimestampNewer",
+			packageVersion: "1744596866",
+			fixedInVersion: "1742843776",
+			repo:           cpeRepo("cpe:/a:redhat:openshift_gitops:1.16::el8"),
+			vulnRepo:       cpeRepo("cpe:/a:redhat:openshift_gitops:1.16::el8"),
+			want:           false,
+		},
+		{
+			name:           "TimestampEqual",
+			packageVersion: "1742843776",
+			fixedInVersion: "1742843776",
+			repo:           cpeRepo("cpe:/a:redhat:openshift_gitops:1.16::el8"),
+			vulnRepo:       cpeRepo("cpe:/a:redhat:openshift_gitops:1.16::el8"),
+			want:           false,
+		},
+		{
+			name:           "TagOlder",
+			packageVersion: "v3.5.5-4",
+			fixedInVersion: "v3.5.7-8",
+			repo:           cpeRepo("cpe:/a:redhat:quay:3::el8"),
+			vulnRepo:       cpeRepo("cpe:/a:redhat:quay:3::el8"),
+			want:           true,
+		},
+		{
+			name:           "GoldenRepoTagOlder",
+			packageVersion: "v3.5.5-4",
+			fixedInVersion: "v3.5.6-1",
+			repo:           &GoldRepo,
+			vulnRepo:       &GoldRepo,
+			want:           true,
+		},
+		{
+			name:           "TagNewer",
+			packageVersion: "v3.5.9-2",
+			fixedInVersion: "v3.5.7-8",
+			repo:           cpeRepo("cpe:/a:redhat:quay:3::el8"),
+			vulnRepo:       cpeRepo("cpe:/a:redhat:quay:3::el8"),
+			want:           false,
+		},
+		{
+			name:           "GoldenRepoTagNewer",
+			packageVersion: "v3.5.7-1",
+			fixedInVersion: "v3.5.6-1",
+			repo:           &GoldRepo,
+			vulnRepo:       &GoldRepo,
+			want:           false,
+		},
+		{
+			name:           "TagCPEMismatch",
+			packageVersion: "v3.5.5-4",
+			fixedInVersion: "v3.5.7-8",
+			repo:           cpeRepo("cpe:/a:redhat:quay:3::el8"),
+			vulnRepo:       cpeRepo("cpe:/a:redhat:openshift:4::el8"),
+			want:           false,
+		},
+		{
+			name:           "GoldenRepoInverted",
+			packageVersion: "7.0.3-13",
+			repo:           &GoldRepo,
+			vulnRepo:       &GoldRepo,
+			invert:         true,
+			want:           true,
+		},
+	}
 
-	t.Run("Normal", func(t *testing.T) {
-		record := &claircore.IndexRecord{
-			Package:    &claircore.Package{Name: "quay/quay-rhel8", Version: "v3.5.5-4"},
-			Repository: &GoldRepo,
-		}
-		vuln := &claircore.Vulnerability{
-			Name:           "CVE-2023-12345",
-			FixedInVersion: "v3.5.6-1",
-			Repo:           &GoldRepo,
-		}
-		got, err := m.Vulnerable(ctx, record, vuln)
-		if err != nil {
-			t.Fatal(err)
-		}
-		if !got {
-			t.Error("expected vulnerable=true when version < fixed")
-		}
-	})
+	var m matcher
+	for _, tc := range table {
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+			record := &claircore.IndexRecord{
+				Package: &claircore.Package{
+					Version: tc.packageVersion,
+				},
+				Repository: tc.repo,
+			}
+			vuln := &claircore.Vulnerability{
+				Invert:         tc.invert,
+				FixedInVersion: tc.fixedInVersion,
+				Repo:           tc.vulnRepo,
+			}
+			got, err := m.Vulnerable(ctx, record, vuln)
+			if err != nil {
+				t.Error(err)
+			}
+			if got != tc.want {
+				t.Errorf("%q failed: Vulnerable(%q, %q) = %v, want %v",
+					tc.name, tc.packageVersion, tc.fixedInVersion, got, tc.want)
+			}
+		})
+	}
+}
 
-	t.Run("Fixed", func(t *testing.T) {
-		record := &claircore.IndexRecord{
-			Package:    &claircore.Package{Name: "quay/quay-rhel8", Version: "v3.5.7-1"},
-			Repository: &GoldRepo,
-		}
-		vuln := &claircore.Vulnerability{
-			Name:           "CVE-2023-12345",
-			FixedInVersion: "v3.5.6-1",
-			Repo:           &GoldRepo,
-		}
-		got, err := m.Vulnerable(ctx, record, vuln)
-		if err != nil {
-			t.Fatal(err)
-		}
-		if got {
-			t.Error("expected vulnerable=false when version >= fixed")
-		}
-	})
+func cpeRepo(s string) *claircore.Repository {
+	w := cpe.MustUnbind(s)
+	return &claircore.Repository{
+		Key:  RepositoryKey,
+		Name: w.String(),
+		CPE:  w,
+	}
 }
