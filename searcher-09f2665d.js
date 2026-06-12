a19570d0b721233a6a15002b43f923faaceb868c
crozzy <crozzy@users.noreply.github.com>
2026-06-12 15:32:39 +0000

    deploy: 8121da3e493952feed87da92173799f7efc2b0e9

--- searcher-09f2665d.js
+++ searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-764345fa.js',
+                path_to_root + 'searchindex-08edab14.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
