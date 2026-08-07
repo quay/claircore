3a7bbe72ac0e741fb10c7826b201ff4a0f6fea66
Hank Donnay <hdonnay@redhat.com>
2026-08-07 17:16:17 -0500

    cpe: add a ragel-based optimized CPE 2.3 unbinding
    Signed-off-by: Hank Donnay <hdonnay@redhat.com>
    Change-Id: I5bf3f39e2302db5684d90903ffb645186a6a6964

--- toolkit/types/cpe/generate.go
+++ toolkit/types/cpe/generate.go
@@ -4,3 +4,4 @@ package cpe
 //go:generate go tool stringer -type ValueKind
 //go:generate go tool stringer -type Relation -linecomment
 //go:generate go tool cpedict
+//go:generate go tool mkragel unbind_fs.rl
