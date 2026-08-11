d7802f2e25a0939187a8e8c0647b140dfa08c31d
Hank Donnay <hdonnay@redhat.com>
2026-08-11 13:03:42 -0500

    cpe: add AppendText implementation
    Signed-off-by: Hank Donnay <hdonnay@redhat.com>
    Change-Id: I3f4f01b86f91ef86918322ee9e8897756a6a6964

--- toolkit/types/cpe/bind.go
+++ toolkit/types/cpe/bind.go
@@ -1,6 +1,11 @@
 package cpe
 
-import "strings"
+import (
+	"encoding"
+	"errors"
+	"strings"
+	"unsafe"
+)
 
 // BindFS returns the WFN bound as CPE 2.3 formatted string.
 func (w WFN) BindFS() string {
@@ -33,3 +38,57 @@ var valueString = strings.NewReplacer(
 	`\-`, `-`,
 	`\_`, `_`,
 )
+
+var (
+	_ encoding.TextAppender = (*Value)(nil)
+	_ encoding.TextAppender = (*WFN)(nil)
+)
+
+// AppendText implements [encoding.TextAppender].
+func (w *WFN) AppendText(b []byte) ([]byte, error) {
+	switch err := w.Valid(); {
+	case err == nil:
+	case errors.Is(err, ErrUnset):
+		return []byte{}, nil
+	default:
+		return nil, err
+	}
+	b = append(b, 'c', 'p', 'e', ':', '2', '.', '3')
+	for i := range NumAttr {
+		// Cannot error
+		b, _ = (&w.Attr[i]).AppendText(b)
+	}
+	return b, nil
+}
+
+// AppendText implements [encoding.TextAppender].
+func (v *Value) AppendText(b []byte) ([]byte, error) {
+	b = append(b, ':')
+	switch v.Kind {
+	case ValueUnset, ValueAny:
+		return append(b, '*'), nil
+	case ValueNA:
+		return append(b, '-'), nil
+	case ValueSet:
+	default:
+		panic("unreachable")
+	}
+
+	esc := false
+	// SAFETY: This is all read-only.
+	for _, c := range unsafe.Slice(unsafe.StringData(v.V), len(v.V)) {
+		switch {
+		case !esc && c == '\\':
+			esc = true
+			continue
+		case esc && (c != '.' && c != '-' && c != '_'):
+			b = append(b, '\\')
+			fallthrough
+		case esc:
+			esc = false
+		default:
+		}
+		b = append(b, c)
+	}
+	return b, nil
+}
