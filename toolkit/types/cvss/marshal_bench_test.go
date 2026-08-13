e158d202eb23e4cc2c5efff1ea3ba01b28ff5064
Hank Donnay <hdonnay@redhat.com>
2026-08-13 15:25:32 -0500

    cvss: benchmarks
    This restructures the benchmarks and adds an `encoding.TextMarshaler`
    benchmark.
    
    Signed-off-by: Hank Donnay <hdonnay@redhat.com>
    Change-Id: I9f83a521ac153eb88af8185ccde0562f6a6a6964

---
+++ toolkit/types/cvss/marshal_bench_test.go
@@ -0,0 +1,130 @@
+package cvss
+
+import (
+	"math/rand/v2"
+	"testing"
+)
+
+func BenchmarkMarshal(b *testing.B) {
+	b.Run("V2", marshalV2)
+	b.Run("V3", marshalV3)
+	b.Run("V4", marshalV4)
+}
+
+func marshalV4(b *testing.B) {
+	benchOne := func(b *testing.B, vec []byte) {
+		b.Helper()
+		b.Attr("input", string(vec))
+		var v V4
+		if err := v.UnmarshalText(vec); err != nil {
+			b.Fatal(err)
+		}
+		var err error
+		var x []byte
+		b.ReportAllocs()
+
+		for b.Loop() {
+			x, err = v.MarshalText()
+			if err != nil {
+				b.Error(err)
+			}
+			_ = x
+		}
+	}
+
+	b.Run("List", func(b *testing.B) {
+		vecs := loadVectorList(b, `v4_roundtrip.list`)
+		todo := make([][]byte, 10)
+		for i := range todo {
+			todo[i] = vecs[rand.N(len(vecs))]
+		}
+		for _, vec := range todo {
+			b.Run("", func(b *testing.B) { benchOne(b, vec) })
+		}
+	})
+	// Each of the following test one fixture plucked from the Spec's examples.
+	b.Run("B", func(b *testing.B) {
+		benchOne(b, []byte("CVSS:4.0/AV:A/AC:H/AT:P/PR:L/UI:P/VC:H/VI:H/VA:H/SC:L/SI:L/SA:L"))
+	})
+	b.Run("BT", func(b *testing.B) {
+		benchOne(b, []byte("CVSS:4.0/AV:A/AC:H/AT:P/PR:L/UI:P/VC:H/VI:H/VA:H/SC:L/SI:L/SA:L/E:P"))
+	})
+	b.Run("BE", func(b *testing.B) {
+		benchOne(b, []byte("CVSS:4.0/AV:L/AC:H/AT:N/PR:N/UI:A/VC:N/VI:N/VA:L/SC:H/SI:H/SA:H/CR:H/IR:H/AR:M/MAV:N/MAC:L/MAT:P/MPR:L/MUI:A/MVC:N/MVI:H/MVA:L/MSC:L/MSI:S/MSA:H"))
+	})
+	b.Run("BTES", func(b *testing.B) {
+		benchOne(b, []byte("CVSS:4.0/AV:N/AC:L/AT:N/PR:H/UI:N/VC:L/VI:L/VA:N/SC:N/SI:N/SA:N/E:U/CR:L/IR:X/AR:L/MAV:A/MAC:H/MAT:N/MPR:N/MUI:P/MVC:X/MVI:N/MVA:H/MSC:N/MSI:L/MSA:S/S:N/AU:N/R:I/V:C/RE:H/U:Green"))
+	})
+}
+
+func marshalV3(b *testing.B) {
+	benchOne := func(b *testing.B, vec []byte) {
+		b.Helper()
+		b.Attr("input", string(vec))
+		var v V3
+		if err := v.UnmarshalText(vec); err != nil {
+			b.Fatal(err)
+		}
+		var err error
+		var x []byte
+		b.ReportAllocs()
+
+		for b.Loop() {
+			x, err = v.MarshalText()
+			if err != nil {
+				b.Error(err)
+			}
+			_ = x
+		}
+	}
+
+	b.Run("List", func(b *testing.B) {
+		vecs := loadVectorList(b, `v31_score.list`)
+		todo := make([][]byte, 10)
+		for i := range todo {
+			todo[i] = vecs[rand.N(len(vecs))]
+		}
+		for _, vec := range todo {
+			b.Run("", func(b *testing.B) { benchOne(b, vec) })
+		}
+	})
+	b.Run("Heartbleed", func(b *testing.B) {
+		benchOne(b, []byte("CVSS:3.1/AV:N/AC:L/PR:N/UI:N/S:U/C:H/I:N/A:N"))
+	})
+}
+
+func marshalV2(b *testing.B) {
+	benchOne := func(b *testing.B, vec []byte) {
+		b.Helper()
+		b.Attr("input", string(vec))
+		var v V2
+		if err := v.UnmarshalText(vec); err != nil {
+			b.Fatal(err)
+		}
+		var err error
+		var x []byte
+		b.ReportAllocs()
+
+		for b.Loop() {
+			x, err = v.MarshalText()
+			if err != nil {
+				b.Error(err)
+			}
+			_ = x
+		}
+	}
+
+	b.Run("List", func(b *testing.B) {
+		vecs := loadVectorList(b, `v2_score.list`)
+		todo := make([][]byte, 10)
+		for i := range todo {
+			todo[i] = vecs[rand.N(len(vecs))]
+		}
+		for _, vec := range todo {
+			b.Run("", func(b *testing.B) { benchOne(b, vec) })
+		}
+	})
+	b.Run("Heartbleed", func(b *testing.B) {
+		benchOne(b, []byte("AV:N/AC:L/Au:N/C:P/I:N/A:N"))
+	})
+}
---
+++ toolkit/types/cvss/unmarshal_bench_test.go
@@ -0,0 +1,113 @@
+package cvss
+
+import (
+	"math/rand/v2"
+	"runtime"
+	"testing"
+)
+
+func BenchmarkUnmarshal(b *testing.B) {
+	b.Run("V2", benchUnmarshalV2)
+	b.Run("V3", benchUnmarshalV3)
+	b.Run("V4", benchUnmarshalV4)
+}
+
+func benchUnmarshalV4(b *testing.B) {
+	benchOne := func(b *testing.B, vec []byte) {
+		b.Helper()
+		b.Attr("input", string(vec))
+		var v V4
+		b.ReportAllocs()
+
+		for b.Loop() {
+			if err := v.UnmarshalText(vec); err != nil {
+				b.Error(err)
+			}
+		}
+		runtime.KeepAlive(&v)
+	}
+
+	b.Run("List", func(b *testing.B) {
+		vecs := loadVectorList(b, `v4_roundtrip.list`)
+		todo := make([][]byte, 10)
+		for i := range todo {
+			todo[i] = vecs[rand.N(len(vecs))]
+		}
+		for _, vec := range todo {
+			b.Run("", func(b *testing.B) { benchOne(b, vec) })
+		}
+	})
+	// Each of the following test one fixture plucked from the Spec's examples.
+	b.Run("B", func(b *testing.B) {
+		benchOne(b, []byte("CVSS:4.0/AV:A/AC:H/AT:P/PR:L/UI:P/VC:H/VI:H/VA:H/SC:L/SI:L/SA:L"))
+	})
+	b.Run("BT", func(b *testing.B) {
+		benchOne(b, []byte("CVSS:4.0/AV:A/AC:H/AT:P/PR:L/UI:P/VC:H/VI:H/VA:H/SC:L/SI:L/SA:L/E:P"))
+	})
+	b.Run("BE", func(b *testing.B) {
+		benchOne(b, []byte("CVSS:4.0/AV:L/AC:H/AT:N/PR:N/UI:A/VC:N/VI:N/VA:L/SC:H/SI:H/SA:H/CR:H/IR:H/AR:M/MAV:N/MAC:L/MAT:P/MPR:L/MUI:A/MVC:N/MVI:H/MVA:L/MSC:L/MSI:S/MSA:H"))
+	})
+	b.Run("BTES", func(b *testing.B) {
+		benchOne(b, []byte("CVSS:4.0/AV:N/AC:L/AT:N/PR:H/UI:N/VC:L/VI:L/VA:N/SC:N/SI:N/SA:N/E:U/CR:L/IR:X/AR:L/MAV:A/MAC:H/MAT:N/MPR:N/MUI:P/MVC:X/MVI:N/MVA:H/MSC:N/MSI:L/MSA:S/S:N/AU:N/R:I/V:C/RE:H/U:Green"))
+	})
+}
+
+func benchUnmarshalV3(b *testing.B) {
+	benchOne := func(b *testing.B, vec []byte) {
+		b.Helper()
+		b.Attr("input", string(vec))
+		var v V3
+		b.ReportAllocs()
+
+		for b.Loop() {
+			if err := v.UnmarshalText(vec); err != nil {
+				b.Error(err)
+			}
+		}
+		runtime.KeepAlive(&v)
+	}
+
+	b.Run("List", func(b *testing.B) {
+		vecs := loadVectorList(b, `v31_score.list`)
+		todo := make([][]byte, 10)
+		for i := range todo {
+			todo[i] = vecs[rand.N(len(vecs))]
+		}
+		for _, vec := range todo {
+			b.Run("", func(b *testing.B) { benchOne(b, vec) })
+		}
+	})
+	b.Run("Heartbleed", func(b *testing.B) {
+		benchOne(b, []byte("CVSS:3.1/AV:N/AC:L/PR:N/UI:N/S:U/C:H/I:N/A:N"))
+	})
+}
+
+func benchUnmarshalV2(b *testing.B) {
+	benchOne := func(b *testing.B, vec []byte) {
+		b.Helper()
+		b.Attr("input", string(vec))
+		var v V2
+		b.ReportAllocs()
+
+		for b.Loop() {
+			if err := v.UnmarshalText(vec); err != nil {
+				b.Error(err)
+			}
+		}
+		runtime.KeepAlive(&v)
+	}
+
+	b.Run("List", func(b *testing.B) {
+		vecs := loadVectorList(b, `v2_score.list`)
+		todo := make([][]byte, 10)
+		for i := range todo {
+			todo[i] = vecs[rand.N(len(vecs))]
+		}
+		for _, vec := range todo {
+			b.Run("", func(b *testing.B) { benchOne(b, vec) })
+		}
+	})
+	b.Run("Heartbleed", func(b *testing.B) {
+		benchOne(b, []byte("AV:N/AC:L/Au:N/C:P/I:N/A:N"))
+	})
+}
